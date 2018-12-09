34c2e06b8cd9819248323240cdbe0403b325eddf
Progoogler <andyzanli@gmail.com>
2018-12-09 13:36:51 +0900

    Update Splash to handle session conditional navigation

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -4,12 +4,75 @@ import {
   Text,
   View,
 } from 'react-native';
+import PropTypes from 'prop-types';
+import debug from '../../utils/debug';
 
-const Splash = () => (
-  <View style={styles.container}>
-    <Text style={styles.logo}>Right On!</Text>
-  </View>
-);
+class Splash extends React.Component {
+
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+
+
+  async componentDidMount() {
+    const { session } = this.props;
+
+    try {
+      await new Promise(async (resolve, reject) => setTimeout(() => {
+        if (!session) {
+          reject('No current session');
+          return;
+        }
+
+        resolve();
+      }, 3000));
+    } catch (exception) {
+      debug.log('Rejected session from Splash:', exception);
+    }
+
+    const loggedIn = session && session.isValid();
+
+    this.setState({ isLoading: false });
+
+    this._navigateTo(loggedIn ? 'Home' : 'OnboardIntroSlides');
+  }
+
+
+
+  _navigateTo(routeName) {
+    this.props.navigation.navigate(routeName);
+  }
+
+
+
+  render() {
+    return (
+      this.state.isLoading &&
+      <View style={styles.container}>
+        <Text style={styles.logo}>Right On!</Text>
+      </View>
+    );
+  }
+}
+
+Splash.propTypes = {
+  navigation: PropTypes.object.isRequired,
+  session: PropTypes.object,
+};
+
+Splash.defaultProps = {
+  navigation: {},
+  session: {},
+};
 
 const styles = StyleSheet.create({
   container: {
