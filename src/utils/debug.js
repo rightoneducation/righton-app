0a8a017686d14426bb6d66554c8f9187a5c25d19
Progoogler <andyzanli@gmail.com>
2018-12-09 12:26:53 +0900

    Port old utils from Firebase project

---
+++ src/utils/debug.js
@@ -0,0 +1,26 @@
+function error() {
+  if (!__DEV__) return;
+  console.error(Array.prototype.join.call(arguments, ' '));
+}
+
+function log() {
+  if (!__DEV__) return;
+  console.log(Array.prototype.join.call(arguments, ' '));
+}
+
+function table(value) {
+  if (!__DEV__) return;
+  console.table(value);
+}
+
+function warn() {
+  if (!__DEV__) return;
+  console.warn(Array.prototype.join.call(arguments, ' '));
+}
+
+export default debug = {
+  error,
+  log,
+  table,
+  warn,
+}
