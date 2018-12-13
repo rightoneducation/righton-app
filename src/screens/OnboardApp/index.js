d238fb0ccd1bf64915eedb0dfff91fa13938f52a
Progoogler <andyzanli@gmail.com>
2018-12-13 19:16:06 +0900

    Replace base First screen w/ Teacher/Student router splash

---
+++ src/screens/OnboardApp/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import OnboardAppRouter from '../OnboardAppRouter';
+import TeacherFirst from '../../Teacher/screens/TeacherFirst';
+import StudentFirst from '../../Student/screens/StudentFirst';
+
+
+const OnboardApp = StackNavigator({
+
+
+
+  OnboardAppRouter: {
+    screen: (props) => {
+      const { navigation } = props;
+
+      return (
+        <OnboardAppRouter
+          onboardNavigator={navigation}
+        />
+      );
+    },
+    navigationOptions: {
+      header: null,
+    },
+  },
+
+
+
+  TeacherFirst: {
+    screen: (props) => {
+      const { screenProps, navigation, ...otherProps } = props;
+
+      return (
+        <TeacherFirst
+          { ...screenProps }
+          { ...otherProps }
+          onLogIn={() => screenProps.rootNavigator.navigate('TeacherApp')}
+        />
+      );
+    },
+    navigationOptions: {
+      header: null,
+    },
+  },
+
+
+
+  StudentFirst: {
+    screen: (props) => {
+      const { screenProps, ...otherProps } = props;
+
+      return (
+        <StudentFirst
+          { ...screenProps }
+          { ...otherProps }
+          onLogIn={() => screenProps.rootNavigator.navigate('StudentApp')}
+        />
+      )
+    },
+    navigationOptions: {
+      header: null,
+    },
+  },
+
+
+
+}, { header: null });
+
+
+export default (props) => {
+  const { screenProps, ...otherProps } = props;
+  return <OnboardApp screenProps={{ ...screenProps, ...otherProps }} />
+};
---
+++ src/screens/OnboardAppRouter/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import Touchable from 'react-native-platform-touchable';
+import { colors, fonts } from '../../utils/theme';
+
+
+export default OnboardAppRouter = ({ onboardNavigator }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.placeholder}>RightOn!</Text>
+      <View style={styles.iamaButtonsContainer}>
+        <Touchable
+          activeOpacity={.8}
+          background={Touchable.Ripple(colors.primary, false)}
+          onPress={() => onboardNavigator.navigate('TeacherFirst')}
+          style={styles.iamaButton}
+        >
+          <View style={styles.iamaView}>
+            <Text style={styles.iama}>I am a</Text>
+            <Text style={styles.role}>Teacher</Text>
+          </View>
+        </Touchable>
+        <Touchable
+          activeOpacity={.8}
+          background={Touchable.Ripple(colors.primary, false)}
+          onPress={() => onboardNavigator.navigate('StudentFirst')}
+          style={styles.iamaButton}
+        >
+          <View style={styles.iamaView}>
+            <Text style={styles.iama}>I am a</Text>
+            <Text style={styles.role}>Student</Text>
+          </View>
+        </Touchable>
+      </View>
+    </View>
+  )
+}
+
+OnboardAppRouter.propTypes = {
+  onboardNavigator: PropTypes.object.isRequired,
+};
+
+OnboardAppRouter.defaultProps = {
+  onboardNavigator: {},
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: colors.dark,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  iama: {
+    color: colors.white,
+    fontSize: fonts.medium,
+  },
+  iamaButton: {
+    backgroundColor: colors.lightGray,
+    borderRadius: 10,
+    flex: .5,
+    marginHorizontal: 5,
+    paddingVertical: 25,
+  },
+  iamaButtonsContainer: {
+    bottom: 15,
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+  },
+  iamaView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  placeholder: {
+    color: colors.white,
+    fontSize: fonts.large,
+  },
+  role: {
+    color: colors.white,
+    fontSize: fonts.large,
+    fontWeight: 'bold',
+  },
+});
