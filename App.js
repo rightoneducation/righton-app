264c58460f9abdfb3c4e3bcb6a90a9dba5321dad
Progoogler <andyzanli@gmail.com>
2018-12-13 19:17:45 +0900

    Configure DrawerNavigator

--- App.js
+++ App.js
@@ -6,19 +6,13 @@ import { DrawerNavigator } from 'react-navigation';
 import { WithAuth } from './lib/Categories/Auth/Components';
 import Amplify from 'aws-amplify';
 import awsmobile from './src/aws-exports';
-import Home from './src/screens/Home';
-import First from './src/screens/First';
+import OnboardApp from './src/screens/OnboardApp';
 import OnboardIntroSlides from './src/screens/OnboardIntroSlides';
 import Splash from './src/screens/Splash';
 
 Amplify.configure(awsmobile);
 
 const App = DrawerNavigator({
-  Home: {
-    screen: (props) => {
-      return <Home rootNavigator={props.navigation} {...props.screenProps } />;
-    },
-  },
   Splash: {
     screen: (props) => {
       return <Splash navigation={props.navigation} { ...props.screenProps } />;
@@ -27,19 +21,19 @@ const App = DrawerNavigator({
       drawerLabel: ' ',
     },
   },
-  OnboardIntroSlides: {
+  OnboardApp: {
     screen: (props) => {
-      return <OnboardIntroSlides rootNavigator={props.navigation} {...props} />;
+      return <OnboardApp rootNavigator={props.navigation} {...props} />;
     }, 
     navigationOptions: { 
       drawerLabel: ' ',
     },
   },
-  First: {
+  OnboardIntroSlides: {
     screen: (props) => {
-      return <First rootNavigator={props.navigation} screenProps={{ ...props.screenProps }} />;
-    },
-    navigationOptions: {
+      return <OnboardIntroSlides rootNavigator={props.navigation} {...props} />;
+    }, 
+    navigationOptions: { 
       drawerLabel: ' ',
     },
   },
