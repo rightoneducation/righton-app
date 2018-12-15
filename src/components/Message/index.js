8c32e556f46b036aefd1eb84df4b8303520ac18a
Progoogler <andyzanli@gmail.com>
2018-12-15 14:15:17 +0900

    Include Message component

---
+++ src/components/Message/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  Animated,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import Touchable from 'react-native-platform-touchable';
+import { colors, fonts } from '../../config/styles';
+
+export default class Message extends React.PureComponent {
+  constructor() { 
+    super();
+    this.opacity = new Animated.Value(0);
+    this.timeout;
+    this.handleTouchClose = this.handleTouchClose.bind(this);
+  }
+
+  render() {
+    const {
+      // closeFunc,
+      bodyStyle,
+      textStyle,
+      // duration,
+      message,
+      // timeout,
+    } = this.props;
+
+    return (
+      <Animated.View style={[{ opacity: this.opacity }, styles.container, bodyStyle]}>
+        <Touchable
+          activeOpacity={.8}
+          background={Touchable.Ripple(colors.black, false)}
+          hitSlop={{top: 10, right: 10, bottom: 10, left: 10}}
+          onPress={this.handleTouchClose}
+        >
+          <Text style={[styles.message, textStyle]}>{ message }</Text>
+        </Touchable>
+      </Animated.View>
+    )
+  }
+
+  componentDidMount() {
+    const { duration, timeout } = this.props;
+    Animated.timing(
+      this.opacity, {
+        duration: duration ? duration : 1500,
+        toValue: 1,
+        useNativeDriver: true,
+      }
+    ).start();
+    this.timeout = setTimeout(() => {
+      this.handleTouchClose();
+    }, timeout ? timeout : 5500);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  handleTouchClose() {
+    const { closeFunc } = this.props;
+    Animated.timing(
+      this.opacity, {
+        duration: 500,
+        toValue: 0,
+        useNativeDriver: true,
+      }
+    ).start(() => {
+      closeFunc();
+    });
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: colors.darkGray,
+    borderRadius: 100,
+    bottom: 40,
+    justifyContent: 'center',
+    marginHorizontal: 15,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    position: 'absolute',
+    zIndex: 10,
+  },
+  message: {
+    color: colors.white,
+    fontSize: fonts.small,
+    textAlign: 'center',
+  },
+});
