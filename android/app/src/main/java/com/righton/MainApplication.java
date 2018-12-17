510d42a132d107b0ac16ee3c79785ee42625e14a
Progoogler <andyzanli@gmail.com>
2018-12-18 01:42:34 +0900

    Include react-native-gesture-handler

--- android/app/src/main/java/com/righton/MainApplication.java
+++ android/app/src/main/java/com/righton/MainApplication.java
@@ -3,6 +3,7 @@ package com.righton;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
             new RNFetchBlobPackage(),
             new RNAWSCognitoPackage(),
             new VectorIconsPackage()
