4702b7f0dece5e98ea4fd4d183bc388257fc5f11
Progoogler <andyzanli@gmail.com>
2018-12-18 11:30:14 +0900

    Include Student game header component

---
+++ src/Student/components/HeaderTeam/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { colors, fonts } from '../../../utils/theme';
+
+export default function HeaderSimple({ team }) {
+  return (
+    <View style={[styles.container, Platform.OS === 'ios' ? styles.iosContainer : styles.androidContainer]}>
+      <Text style={styles.team}>{ team }</Text>
+      <Text style={styles.team}>RIGHT ON!</Text>
+    </View>
+  );
+}
+
+HeaderSimple.propTypes = {
+  team: PropTypes.string,
+};
+
+HeaderSimple.defaultProps = {
+  team: '',
+};
+
+const styles = StyleSheet.create({
+  androidContainer: {
+    alignItems: 'center',
+  },
+  container: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    height: 45,
+  },
+  iosContainer: {
+    alignItems: 'flex-end',
+    paddingBottom: 10,
+  },
+  team: {
+    color: colors.white,
+    fontSize: fonts.medium,
+  },
+});
