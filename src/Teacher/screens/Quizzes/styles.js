3d8da71d04e70c3d22592fd14e4059ab58b77c34
Progoogler <andyzanli@gmail.com>
2018-12-23 19:27:08 +0900

    Separate styles from index

---
+++ src/Teacher/screens/Quizzes/styles.js
@@ -0,0 +1,63 @@
+import { StyleSheet } from 'react-native';
+import { colors, deviceWidth, fonts } from '../../../utils/theme';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: colors.dark,
+    flex: 1,
+  },
+  header: {
+    alignSelf: 'stretch',
+    backgroundColor: colors.primary,
+    height: 200,
+  },
+  headerTitle: {
+    color: colors.white,
+    fontSize: fonts.large,
+    marginTop: 50,
+    textAlign: 'center',
+  },
+  image: {
+    height: 150,
+    width: 150,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    backgroundColor: colors.lightGray,
+    height: 150,
+    justifyContent: 'center',
+    width: 150,
+  },
+  imageLabel: {
+    color: colors.dark,
+    fontSize: fonts.small,
+    fontStyle: 'italic',
+  },
+  plusButton: {
+    bottom: 15,
+    position: 'absolute',
+    right: 15,
+  },
+  plusIcon: {
+    color: colors.white,
+    fontSize: 25,
+  },
+  quizButton: {
+    backgroundColor: colors.white,
+    flexDirection: 'row',
+    height: 150,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    width: deviceWidth - 30,
+  },
+  quizTitle: {
+    color: colors.dark,
+    fontSize: fonts.medium,
+    padding: 10
+  },
+  scrollview: {
+    flexGrow: 1,
+    marginTop: 15,
+    paddingBottom: 25,
+  },
+});
