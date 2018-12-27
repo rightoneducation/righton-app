02b25ae8f09f62e5fca12a3c726202426177735b
Progoogler <andyzanli@gmail.com>
2018-12-27 22:46:43 +0900

    disable eslint

--- amplify/backend/function/RightOnLambda1/src/app.js
+++ amplify/backend/function/RightOnLambda1/src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
