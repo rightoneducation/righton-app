dad53bd48e9fca110e378a842700ffffba67dbbf
Progoogler <andyzanli@gmail.com>
2019-01-03 00:34:52 +0900

    Update TeacherGameLambda REST api

--- amplify/backend/function/TeacherGameLambda/src/app.js
+++ amplify/backend/function/TeacherGameLambda/src/app.js
@@ -51,45 +51,11 @@ const convertUrlType = (param, type) => {
   }
 }
 
-/********************************
- * HTTP Get method for list objects *
- ********************************/
-
-app.get(path + hashKeyPath, function(req, res) {
-  var condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
-  
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
-
-  let queryParams = {
-    TableName: tableName,
-    KeyConditions: condition
-  } 
-
-  dynamodb.query(queryParams, (err, data) => {
-    if (err) {
-      res.json({error: 'Could not load items: ' + err});
-    } else {
-      res.json(data.Items);
-    }
-  });
-});
-
 /*****************************************
  * HTTP Get method for get single object *
  *****************************************/
 
-app.get(path, function(req, res) {
+app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
   var params = {};
   if (userIdPresent && req.apiGateway) {
     params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
@@ -109,7 +75,12 @@ app.get(path, function(req, res) {
     }
   }
 
-  dynamodb.get(path, (err, data) => {
+  let getItemParams = {
+    TableName: tableName,
+    Key: params
+  }
+
+  dynamodb.get(getItemParams,(err, data) => {
     if(err) {
       res.json({error: 'Could not load items: ' + err.message});
     } else {
@@ -173,7 +144,7 @@ app.post(path, function(req, res) {
 * HTTP remove method to delete object *
 ***************************************/
 
-app.delete(path, function(req, res) {
+app.delete(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
   var params = {};
   if (userIdPresent && req.apiGateway) {
     params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
@@ -193,7 +164,12 @@ app.delete(path, function(req, res) {
     }
   }
 
-  dynamodb.delete(path, (err, data)=> {
+  let removeItemParams = {
+    TableName: tableName,
+    Key: params,
+  }
+
+  dynamodb.delete(removeItemParams, (err, data)=> {
     if(err) {
       res.json({error: err, url: req.url});
     } else {
