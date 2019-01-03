558dd9a0d8604ae44fe2b35e67e64a778d59227e
Progoogler <andyzanli@gmail.com>
2019-01-03 21:13:55 +0900

    Commit data schema document

---
+++ src/config/schema.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+const gameState = {
+  avatar: 'string',
+  title: 'string',
+  description: 'string',
+  'group#': [{ /* question schema */
+    answer: 'string',
+    image: 'string',
+    instructions: ['string'],
+    question: 'string',
+    uid: 'string',
+    group: 'number', // ??
+    tricks: ['string'],
+  }],
+  GameRoomID: 'string',
+  groups: 'number',
+  answering: 'number', // index of quiz in questions array
+}
