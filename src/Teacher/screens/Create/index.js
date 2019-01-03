544e57a9ee3ac11e32570aecc963426a5309da87
Progoogler <andyzanli@gmail.com>
2019-01-03 21:13:30 +0900

    Remove GroupSelection view & handle IoT and setGameState in QuizSelection

--- src/Teacher/screens/Create/index.js
+++ src/Teacher/screens/Create/index.js
@@ -9,8 +9,7 @@ import {
   View,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import { IOTSubscribeToTopic, unsubscribeFromTopic, publishMessage } from '../../../../lib/Categories/IoT';
-import { deleteGameFromDynamoDB, getGameFromDynamoDB, putGameToDynamoDB } from '../../../../lib/Categories/DynamoDB/TeacherAPI';
+import { getGameFromDynamoDB, putGameToDynamoDB } from '../../../../lib/Categories/DynamoDB/TeacherAPI';
 import Swiper from 'react-native-swiper';
 import Touchable from 'react-native-platform-touchable';
 import Portal from '../../../screens/Portal';
@@ -29,14 +28,20 @@ const blockSize = deviceWidth / 4;
 class Create extends React.Component {
   static propTypes = {
     screenProps: PropTypes.shape({
+      handleSetGameState: PropTypes.func.isRequired,
+      IOTPublishMessage: PropTypes.func.isRequired,
+      IOTSubscribeToTopic: PropTypes.func.isRequired,
       navigation: PropTypes.shape({
-        navigate: PropTypes.func,
+        navigate: PropTypes.func.isRequired,
       }),
     }),
   };
   
   static defaultProps = {
     screenProps: {
+      handleSetGameState: () => {},
+      IOTPublishMessage: () => {},
+      IOTSubscribeToTopic: () => {},
       navigation: {
         navigate: () => {},
       },
@@ -47,7 +52,6 @@ class Create extends React.Component {
     super(props);
 
     this.hydratedQuizzes = false;
-    this.gamePutInDynamoDB = false;
 
     this.state = {
       activeQuiz: {},
@@ -57,21 +61,10 @@ class Create extends React.Component {
 
     this.handleRoomInput = this.handleRoomInput.bind(this);
     this.handleRoomSubmit = this.handleRoomSubmit.bind(this);
-    this.handleGroupSelection = this.handleGroupSelection.bind(this);
+    // this.handleGroupSelection = this.handleGroupSelection.bind(this);
 
     this.handleBackFromGroups = this.handleBackFromGroups.bind(this);
     this.handleBackFromHost = this.handleBackFromHost.bind(this);
-
-    this.handleReceivedMessage = this.handleReceivedMessage.bind(this);
-  }
-  
-  
-  componentWillUnmount() {
-    const { room } = this.state;
-    // TODO Handle this in top navigator state...
-    // Keep state of game and whether this needs to happen on app close or not.
-    if (this.gamePutInDynamoDB) deleteGameFromDynamoDB(room, r => debug.log('R', r), e => debug.log('E', e));
-    unsubscribeFromTopic(room);
   }
 
 
@@ -113,20 +106,20 @@ class Create extends React.Component {
 
     getGameFromDynamoDB(room,
       (res) => {
-        if (res && res.username && username !== res.username) {
+        if (res && (res.username && username !== res.username)) {
+          debug.log('Invalid access!', 'Required username:', res.username, 'Actual username:', username);
           // Invalid teacher account -- forbid access!
           setTimeout(() => this.swiperRef.scrollBy(-1, false), 500);
           // TODO Send message that account error / create room w/ different name
         } else if (res && (username === res.username || res.username === null)) {
           debug.log('Username matches and game room still exists: Enter');
           setTimeout(() => this.swiperRef.scrollBy(1, false), 500);
-          this.gamePutInDynamoDB = true;
         } else if (!res || (res && !res.GameRoomId)) {
+          debug.log('GameRoom w/ ID does not exist: Create & Enter');
           putGameToDynamoDB(room, username,
             (putRes) => {
               setTimeout(() => this.swiperRef.scrollBy(1, false), 500);
               debug.log('Put game in DynamoDB!', putRes);
-              this.gamePutInDynamoDB = true;
             },
             (exception) => {
               setTimeout(() => this.swiperRef.scrollBy(1, false), 500);
@@ -154,51 +147,41 @@ class Create extends React.Component {
 
 
   handleQuizSelection(e, quiz) {
-    this.setState({ activeQuiz: quiz });
-    this.swiperRef.scrollBy(1, false);
-  }
+    const { room } = this.state;
 
+    const groupQuestions = {};
+    quiz.questions.forEach((question, idx) => {
+      groupQuestions[`group${idx}`] = question;
+    });
 
-  async handleGroupSelection(number) {
-    const { activeQuiz, room } = this.state;
-    const awsQuiz = {
-      ...activeQuiz,
-      groups: number,
+    const gameState = {
+      GameRoomID: room,
+      avatar: quiz.avatar,
+      title: quiz.description,
+      description: quiz.description,
+      ...groupQuestions,
+      groups: quiz.questions.length,
       answering: null,
-      uid: `${Number.random()}`,
     };
-    const data = {
-      action: 'SET_QUIZ_STATE',
-      data: { ...awsQuiz },
-    };
-    // const schema = {
-    //   avatar: 'string',
-    //   title: 'string',
-    //   description: 'string',
-    //   group#: [{ /* question schema */
-    //     answer: 'string',
-    //     image: 'string',
-    //     instructions: ['string'],
-    //     question: 'string',
-    //     uid: 'string',
-    //     group: 'number', ??
-    //     tricks: ['string'],
-    //   }],
-    //   GameRoomID: 'string',
-    //   groups: 'number',
-    //   answering: 'number', // index of quiz in questions array
-    // };
+    
+    this.props.screenProps.handleSetGameState(gameState);
+    
+    this.props.screenProps.IOTSubscribeToTopic(room);
 
-    IOTSubscribeToTopic(room, this.handleReceivedMessage);
-    setTimeout(() => {
-      const message = JSON.stringify(data);
-      publishMessage(room, message);
-    }, 5000);
-  }
+    // TODO! Handle navigating to Teacher Game Room.
 
-
-  handleReceivedMessage = (message) => {
-    debug.log('Received Message', message);
+    // if (__DEV__) {
+    //   setTimeout(() => {
+    //     const uid = `${Math.random()}`;
+    //     const data = {
+    //       action: 'REQUEST_GAME_STATE',
+    //       uid,
+    //       gameState,
+    //     };
+    //     const message = JSON.stringify(data);
+    //     this.props.screenProps.IOTPublishMessage(message, uid);
+    //   }, 5000);
+    // }
   }
 
 
@@ -229,31 +212,31 @@ class Create extends React.Component {
   }
 
 
-  renderNumberBlock = number => (
-    <Touchable
-      activeOpacity={0.8}
-      background={Touchable.Ripple(colors.primary, false)}
-      key={`${number}`}      
-      onPress={() => this.handleGroupSelection(number)}
-      style={styles.blockContainer}
-    >
-      <Text style={styles.blockNumber}>{ number }</Text>
-    </Touchable>
-  );
+  // renderNumberBlock = number => (
+  //   <Touchable
+  //     activeOpacity={0.8}
+  //     background={Touchable.Ripple(colors.primary, false)}
+  //     key={`${number}`}      
+  //     onPress={() => this.handleGroupSelection(number)}
+  //     style={styles.blockContainer}
+  //   >
+  //     <Text style={styles.blockNumber}>{ number }</Text>
+  //   </Touchable>
+  // );
 
 
-  renderNumberBlocks = () => {
-    const { activeQuiz } = this.state;
-    if (!activeQuiz.questions) return null;
-    const len = activeQuiz.questions.length;
-    const arr = [];
-    arr[len] = undefined;
-    return (
-      <View style={styles.blocksContainer}>
-        {arr.map((n, idx) => len % idx === 0 && this.renderNumberBlock(idx))}
-      </View>
-    );
-  }
+  // renderNumberBlocks = () => {
+  //   const { activeQuiz } = this.state;
+  //   if (!activeQuiz.questions) return null;
+  //   const len = activeQuiz.questions.length;
+  //   const arr = [];
+  //   arr[len] = undefined;
+  //   return (
+  //     <View style={styles.blocksContainer}>
+  //       {arr.map((n, idx) => len % idx === 0 && this.renderNumberBlock(idx))}
+  //     </View>
+  //   );
+  // }
 
 
   render() {
@@ -310,7 +293,7 @@ class Create extends React.Component {
         </ScrollView>
 
 
-        <ScrollView contentContainerStyle={[firstStyles.container, styles.scrollview]}>
+        {/* <ScrollView contentContainerStyle={[firstStyles.container, styles.scrollview]}>
           <ButtonBack
             onPress={this.handleBackFromGroups}
           />
@@ -320,7 +303,7 @@ class Create extends React.Component {
             label={'Launch Game'}
             onPress={this.handleGroupSelection}
           />
-        </ScrollView>
+        </ScrollView> */}
 
 
       </Swiper>
