b4283d84c2df94cb435e53d34c06ea58c31d72a8
Progoogler <andyzanli@gmail.com>
2019-01-08 15:14:47 +0900

    Include choices array and correctChoice int in schema

--- src/config/schema.js
+++ src/config/schema.js
@@ -6,6 +6,8 @@ const gameState = {
   description: 'string',
   'team#': [{ /* question schema */
     answer: 'string',
+    choices: ['object'], // Object references to tricks[index]
+    correctChoice: 'number', // index in choices which references to the correct answer
     image: 'string',
     instructions: ['string'],
     question: 'string',
