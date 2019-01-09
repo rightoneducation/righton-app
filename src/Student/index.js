c25a7fbe4b27cc2cd459468581b5dc49679fcbbf
Progoogler <andyzanli@gmail.com>
2019-01-09 11:54:06 +0900

    Include GameQuiz in Student Navigation

--- src/Student/index.js
+++ src/Student/index.js
@@ -3,6 +3,7 @@ import { createSwitchNavigator } from 'react-navigation';
 import Dashboard from './screens/Dashboard';
 import GamePreview from './screens/GamePreview';
 import GameTricks from './screens/GameTricks';
+import GameQuiz from './screens/GameQuiz';
 
 const StudentSwitchNavigator = createSwitchNavigator({
 
@@ -30,6 +31,16 @@ const StudentSwitchNavigator = createSwitchNavigator({
   GameTricks: {
     screen: props => (
       <GameTricks screenProps={{ ...props.screenProps }} navigation={props.navigation} />
+    ),
+    navigationOptions: {
+
+    },
+  },
+
+
+  GameQuiz: {
+    screen: props => (
+      <GameQuiz screenProps={{ ...props.screenProps }} navigation={props.navigation} />
     ),
     navigationOptions: {
 
