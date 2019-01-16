f4f212f1c536f41b34a682f3ce3a30c2c5847306
Progoogler <andyzanli@gmail.com>
2019-01-16 17:45:43 +0800

    Include additional mock schemas

--- src/config/schema.js
+++ src/config/schema.js
@@ -1,5 +1,12 @@
 /* eslint-disable */
 
+
+const GameRoom = {
+  GameRoomID: 'number',
+  date: 'number',
+};
+
+
 const gameState = {
   answering: 'number', // index of quiz in questions array
   banner: 'string',
@@ -19,4 +26,39 @@ const gameState = {
   }],
   GameRoomID: 'string',
   state: 'object',
+};
+
+
+const TeacherAccount = {
+  email: '',
+  games: ['object'],
+  favorites: ['object'],
+  history: ['object'],  // Depending on data stored we can generate reports of students engagement over time
+                        // - number of correct answers per game (as bars)
+                        // - number of trick answers per game (as bars)
+                        // - tricked to correct ratio performance per game
+  // settings: {}
+}
+
+
+const StudentAccount = {
+  email: 'string',
+  gamesPlayed: 'number',
+  playersTricked: 'number',
+  tricksSuggested: 'number',
+  points: 'number',
+}
+
+
+const QuizMaker = {
+  answer: 'string',
+  category: 'string',
+  question: 'string',
+  played: 'number', // To calculate popularity rating
+  tricks: [
+    {
+      votes: 'number',
+      value: 'string',
+    }
+  ],
 }
