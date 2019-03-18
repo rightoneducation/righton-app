ef18085f692243398326fcb721ea666371feea9a
Progoogler <andyzanli@gmail.com>
2019-03-18 15:48:10 -0700

    Change function component to class & fix rendering of team

--- src/Teacher/screens/GameRoom/GameRoomPreview.js
+++ src/Teacher/screens/GameRoom/GameRoomPreview.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { gameStatePropTypes, gameStateDefaultProps } from '../../../config/propTypes';
+import { cancelCountdownTimer, requestCountdownTimer } from '../../../utils/countdownTimer';
 // import Aicon from 'react-native-vector-icons/FontAwesome';
 import KeepAwake from 'react-native-keep-awake';
 import ButtonBack from '../../../components/ButtonBack';
@@ -15,79 +16,124 @@ import ButtonWide from '../../../components/ButtonWide';
 import Touchable from 'react-native-platform-touchable';
 import gamePreviewStyles from '../../../Student/screens/GamePreview/styles';
 
-export default function GameRoomPreview({
-  gameState,
-  handleBackFromChild,
-  handleStartQuiz,
-  handleViewResults,
-  teamRef,
-}) {
-  const startedQuiz = gameState.state.startQuiz;
+export default class GameRoomPreview extends React.Component {
+  static propTypes = {
+    gameState: gameStatePropTypes,
+    handleBackFromChild: PropTypes.func.isRequired,
+    handleViewResults: PropTypes.func.isRequired,
+    handleStartQuiz: PropTypes.func.isRequired,
+    teamRef: PropTypes.string.isRequired,
+  }
+  
+  static defaultProps = {
+    gameState: gameStateDefaultProps,
+    handleBackFromChild: () => {},  
+    handleViewResults: () => {},
+    handleStartQuiz: () => {},
+    teamRef: 'team0',
+  }
 
-  return (
-    <ScrollView
-      contentContainerStyle={gamePreviewStyles.container}
-    >
-      { Platform.OS === 'ios' && <KeepAwake /> }
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeLeft: props.gameState.quizTime && props.gameState.quizTime !== '0:00' ?
+        props.gameState.quizTime : 'No time limit',
+    };
+  }
 
-      <ButtonBack
-        onPress={handleBackFromChild}
-      />
+  componentDidMount() {
+    const { gameState } = this.props;
+    if (gameState.state.startQuiz) {
+      if (gameState.quizTime && gameState.quizTime !== '0:00') {
+        requestCountdownTimer(gameState.quizTime, this.setTime);
+      }
+    }
+  }
 
-      <Text styles={[gamePreviewStyles.timeContainer, gamePreviewStyles.teamContainer]}>{ `Team ${teamRef + 1}` }</Text>
-      
-      <View 
-        style={[gamePreviewStyles.questionContainer, gamePreviewStyles.questionContainerTeacher]}
-      >
-        <Text style={gamePreviewStyles.question}>{ gameState[teamRef].question }</Text>
-        {Boolean(gameState[teamRef].image) &&
-          <Image source={{ uri: gameState[teamRef].image }} style={gamePreviewStyles.image} resizeMode={'contain'} />} 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.gameState.state.startQuiz !== nextProps.gameState.state.startQuiz) {
+      if (nextProps.gameState.state.startQuiz) {
+        requestCountdownTimer(nextProps.gameState.quizTime, this.setTime);
+      } else {
+        cancelCountdownTimer();
+        this.setTime('Time is up!');
+      }
+    }
+  }
+
+  setTime = timeLeft => this.setState({ timeLeft });
+
+  render() {
+    const { 
+      gameState,
+      handleBackFromChild,
+      handleStartQuiz,
+      handleViewResults,
+      teamRef,
+    } = this.props;
+
+    const { timeLeft } = this.state;
+
+    const startedQuiz = gameState.state.startQuiz;
+
+    return (
+      <View style={gamePreviewStyles.flex}>
+        {Boolean(timeLeft) &&
+          <Text style={gamePreviewStyles.timeContainer}>{ timeLeft }</Text>}
+        <ScrollView
+          contentContainerStyle={gamePreviewStyles.container}
+        >
+          { Platform.OS === 'ios' && <KeepAwake /> }
+
+          <ButtonBack
+            onPress={handleBackFromChild}
+          />
+
+          <Text styles={[gamePreviewStyles.timeContainer, gamePreviewStyles.teamContainer]}>
+            { `Team ${parseInt(teamRef.substring(4), 10) + 1}` }
+          </Text>
+          
+          <View 
+            style={[
+              gamePreviewStyles.questionContainer,
+              gamePreviewStyles.questionContainerTeacher,
+            ]}
+          >
+            <Text style={gamePreviewStyles.question}>{ gameState[teamRef].question }</Text>
+            {Boolean(gameState[teamRef].image) &&
+              <Image source={{ uri: gameState[teamRef].image }} style={gamePreviewStyles.image} resizeMode={'contain'} />} 
+          </View>
+          <View style={gamePreviewStyles.choiceContainerWrapper}>
+            <View style={gamePreviewStyles.choicesContainer}>
+              {gameState[teamRef].choices.map(choice => (
+                <Touchable
+                  activeOpacity={0.8}
+                  key={choice.uid}
+                  onPress={() => {}}
+                >
+                  <View style={gamePreviewStyles.choiceContainer}>
+                    <View style={[gamePreviewStyles.choiceButton]}>
+                      {choice.selected &&
+                        <View style={gamePreviewStyles.choiceButton} />}
+                    </View>
+                    <Text style={gamePreviewStyles.choiceValue}>{ choice.value }</Text>
+                  </View>
+                </Touchable>
+              ))}
+            </View>
+          </View>
+          {startedQuiz ?
+            <ButtonWide
+              label={'View results'}
+              onPress={() => handleViewResults(teamRef)}
+            />
+            :
+            <ButtonWide
+              label={'Start quiz'}
+              onPress={() => handleStartQuiz(teamRef)}
+            />}
+        </ScrollView>
       </View>
-      <View style={gamePreviewStyles.choiceContainerWrapper}>
-        <View style={gamePreviewStyles.choicesContainer}>
-          {gameState[teamRef].choices.map(choice => (
-            <Touchable
-              activeOpacity={0.8}
-              key={choice.uid}
-              onPress={() => {}}
-            >
-              <View style={gamePreviewStyles.choiceContainer}>
-                <View style={[gamePreviewStyles.choiceButton]}>
-                  {choice.selected &&
-                    <View style={gamePreviewStyles.choiceButton} />}
-                </View>
-                <Text style={gamePreviewStyles.choiceValue}>{ choice.value }</Text>
-              </View>
-            </Touchable>
-          ))}
-        </View>
-      </View>
-      {startedQuiz ?
-        <ButtonWide
-          label={'View results'}
-          onPress={() => handleViewResults(teamRef)}
-        />
-        :
-        <ButtonWide
-          label={'Start quiz'}
-          onPress={() => handleStartQuiz(teamRef)}
-        />}
-    </ScrollView>
-  );
+    );
+  }
 }
-
-GameRoomPreview.propTypes = {
-  gameState: gameStatePropTypes,
-  handleBackFromChild: PropTypes.func.isRequired,
-  handleViewResults: PropTypes.func.isRequired,
-  handleStartQuiz: PropTypes.func.isRequired,
-  teamRef: PropTypes.string.isRequired,
-};
-
-GameRoomPreview.defaultProps = {
-  gameState: gameStateDefaultProps,
-  handleBackFromChild: () => {},  
-  handleViewResults: () => {},
-  handleStartQuiz: () => {},
-  teamRef: 'team0',
-};
