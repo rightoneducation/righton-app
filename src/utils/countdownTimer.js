5ac2a141b5eb99484b8438f3c0a44902d04cfea3
Progoogler <andyzanli@gmail.com>
2019-04-11 23:53:43 -0700

    Refactor utility to transformTimeToString() and include startCountdownTimer as export

--- src/utils/countdownTimer.js
+++ src/utils/countdownTimer.js
@@ -1,12 +1,14 @@
+/* eslint no-undef: 0 */
+
 let REQUEST = null;
+let CURRENT_END_TIME = 0;
 
 export function requestCountdownTimer(endTime, callback) {
   // set our end time
-  let actualEndTime = null;
   if (typeof endTime === 'string') {
-    actualEndTime = transformStringToTime(endTime);
+    CURRENT_END_TIME = transformStringToTime(endTime);
   } else {
-    actualEndTime = endTime;
+    CURRENT_END_TIME = endTime;
   }
 
   // calculate remaining time from now until deadline
@@ -14,21 +16,15 @@ export function requestCountdownTimer(endTime, callback) {
     const currentTime = new Date().getTime();
     return deadline - currentTime;
   }
-  
-  // pad value with zero
-  function pad(value) {
-    return ('0' + Math.floor(value)).slice(-2);
-  }
 
   // show time repeatedly
   function showTime() {
-    const remainingTime = getRemainingTime(actualEndTime);
-    const seconds = pad((remainingTime / 1000) % 60);
-    const minutes = pad((remainingTime / (60 * 1000)) % 60);
+    const remainingTime = getRemainingTime(CURRENT_END_TIME);
+    const newTime = transformTimeToString(remainingTime);
 
-    const newTime = `${minutes}:${seconds}`;
     if (newTime === '00:00') {
       callback('Time is up!');
+      CURRENT_END_TIME = 0;
       return;
     }
     callback(newTime);
@@ -38,17 +34,48 @@ export function requestCountdownTimer(endTime, callback) {
       REQUEST = requestAnimationFrame(showTime);
     }
   }
-  
-  // kick it all off
-  requestAnimationFrame(showTime);
-};
 
-export function cancelCountdownTimer() {
-  cancelAnimationFrame(REQUEST);
+  requestAnimationFrame(showTime);
 }
+
 
 function transformStringToTime(timeString = '00:00') {
   const minutes = parseInt(timeString.substr(0, timeString.indexOf(':')), 10);
   const seconds = parseInt(timeString.substr(timeString.indexOf(':') + 1), 10);
   return new Date().getTime() + (minutes * 60000) + (seconds * 1000);
+}
+
+
+function transformTimeToString(remainingTime) {
+  const seconds = pad((remainingTime / 1000) % 60);
+  const minutes = pad((remainingTime / (60 * 1000)) % 60);
+  const newTime = `${minutes}:${seconds}`;
+  return newTime;
+}
+
+
+// pad value with zero
+function pad(value) {
+  return (`0${Math.floor(value)}`).slice(-2);
+}
+
+
+export function startCountdownTimer(params, defaultTime, setTime) {
+  const timeLeft = (params && params.time) ? params.time : defaultTime;
+  if (timeLeft && (timeLeft !== '0:00' && timeLeft !== 'No time limit')) {
+    requestCountdownTimer(timeLeft, setTime);
+  }
+}
+
+
+export function cancelCountdownTimer() {
+  cancelAnimationFrame(REQUEST);
+  CURRENT_END_TIME = 0;
+}
+
+
+export function getCurrentEndTime() {
+  if (CURRENT_END_TIME === 0) return '0:00';
+  const now = new Date().getTime();
+  return transformTimeToString(CURRENT_END_TIME - now);
 }
