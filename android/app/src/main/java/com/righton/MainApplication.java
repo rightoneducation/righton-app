f10e0de5fc524e771ea083aad1e33430ced233f3
Progoogler <andyzanli@gmail.com>
2019-04-19 17:13:47 -0700

    Link react-native code push and react-native-autogrowing-textinput

--- android/app/src/main/java/com/righton/MainApplication.java
+++ android/app/src/main/java/com/righton/MainApplication.java
@@ -3,6 +3,8 @@ package com.righton;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
+import com.wix.autogrowtextinput.AutoGrowTextInputPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -19,6 +21,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -28,6 +36,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+            new AutoGrowTextInputPackage(),
             new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
