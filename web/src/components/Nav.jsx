717690cf52a404e4f943dea7fe7423b68ef1aef6
zach2025 <zachthomas2025@gmail.com>
2022-02-11 14:44:23 -0500

    Updated necessary import

--- web/src/components/Nav.jsx
+++ web/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, useRouteMatch } from "react-router-dom";
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { alpha, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
