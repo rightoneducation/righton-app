6392088b2ab1e38e44cbc190d7bc8211a63c3211
Jared <jmj379@drexel.edu>
2022-02-21 16:54:22 -0500

    Update question ui

--- web/src/components/QuestionForm.jsx
+++ web/src/components/QuestionForm.jsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, Divider, List, ListItem, Button, Grid, Card, CardContent, Paper, Collapse, IconButton } from '@material-ui/core';
-import { ExpandMore, Add, Place } from '@material-ui/icons';
+import { Typography, TextField, Divider, Button, Select, MenuItem } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 import Placeholder from '../images/RightOnPlaceholder.svg';
+import QuestionFormAnswerDropdown from './QuestionFormAnswerDropdown';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import QuestionIcon from './QuestionIcon';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -12,16 +14,13 @@ const useStyles = makeStyles(theme => ({
     maxWidth: '75%',
     marginLeft: 200,
   },
-
   input: {
     margin: `${theme.spacing(2)}px 0`,
     width: '100%',
   },
-
   half: {
     width: '50%'
   },
-
   imagePreview: {
     padding: 0,
     display: 'inline-block',
@@ -32,22 +31,18 @@ const useStyles = makeStyles(theme => ({
     width: '375px',
     height: '290px',
   },
-
   image: {
     maxWidth: '375px',
     maxHeight: '285px',
     display: 'block',
     marginLeft: 'auto'
   },
-
   divider: {
     margin: `${theme.spacing(2)}px 0`,
   },
-
   number: {
     width: '40px',
   },
-
   deleteButton: {
     background: 'rgba(0, 0, 0, 0.12)',
     borderRadius: 18,
@@ -60,15 +55,14 @@ const useStyles = makeStyles(theme => ({
     top: -40,
     right: 30,
   },
-
   back: {
-    marginLeft: theme.spacing(2),
+    position: 'absolute',
+    top: 100,
+    left: 0,
   },
-
   instruction: {
     padding: 0,
   },
-
   addURLButton: {
     background: 'linear-gradient(90deg, #0A4178 0%, #0F56A1 100%)',
     borderRadius: 50,
@@ -77,64 +71,45 @@ const useStyles = makeStyles(theme => ({
     top: 370,
     right: 95,
   },
-
   greenButton: {
     background: 'linear-gradient(90deg, #4DED66 0%, #5ACD3D 100%)',
     borderRadius: 50,
     color: 'white',
     fontSize: 17,
   },
-
   addGameButton: {
     color: 'white',
-    fontSize: 17,
-    background: 'linear-gradient(90deg, #159EFA 0%, #19BCFB 100%)',
-    borderRadius: 50,
+    fontSize: 16,
+    background: 'linear-gradient(90deg, #4DED66 0%, #5ACD3D 100%)',
+    borderRadius: 60,
+    width: 170,
+    height: 58
   },
-
-  card: {
-    borderRadius: '10px',
-    boxShadow: '1px 4px 10px lightgrey',
-    width: '100%',
-    marginBottom: '20px',
+  dropdown: {
+    width: 120,
+    height: 44,
+    background: '#FFFFFF',
+    border: '1px solid #BDBDBD',
+    boxSizing: 'border-box',
+    borderRadius: '8px',
+    padding: 20,
+    fontSize: 20,
+    // '& .MuiSvgIcon-root': {
+    //   Use this to change arrow CSS
+    // },
   },
-
-  correctCard: {
-    borderRadius: '10px',
-    boxShadow: '1px 4px 10px lightgrey',
-    width: '85%',
-    border: '5px solid #4DED66',
-    marginBottom: '20px',
+  dropdownWrapper: {
+    textAlign: 'left',
+    '& p': {
+      fontSize: '14px', 
+      fontWeight: 700,
+    }
   },
-
-  answer: {
-    fontWeight: 500,
-    color: '#384466',
-    fontSize: '18px',
-  },
-
-  expand: {
-    float: 'right',
-    color: '#384466',
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-  },
-
-  expanded: {
-    float: 'right',
-    color: '#384466',
-    transform: 'rotate(180deg)',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-  },
-
-  explanationTitle: {
-    fontWeight: 700,
-    fontSize: '20px',
-    color: '#384466',
+  MenuProps: {
+    '& .MuiMenuItem-root.Mui-selected': {
+      color: 'white',
+      background: 'linear-gradient(90deg, #159EFA 0%, #19BCFB 100%)',
+    },
   },
 }));
 
@@ -148,6 +123,7 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
     text: '',
     imageUrl: '',
     answer: '',
+    wrongAnswers: [{choice: "Choice0", explanation: "Explanation0"}, {choice: "Choice1", explanation: "Explanation1"}, {choice: "Choice2", explanation: "Explanation2"}],
     instructions: [],
     gameId
   });
@@ -158,13 +134,16 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
         originalQuestion.instructions = JSON.parse(originalQuestion.instructions);
         originalQuestion.instructions = JSON.parse(originalQuestion.instructions);
       }
-      setQuestion(originalQuestion)
+      if (originalQuestion.wrongAnswers !== null && originalQuestion.wrongAnswers !== [] && typeof originalQuestion.wrongAnswers === 'string') {
+        originalQuestion.wrongAnswers = JSON.parse(originalQuestion.wrongAnswers);
+        originalQuestion.wrongAnswers = JSON.parse(originalQuestion.wrongAnswers);
+      }
+      setQuestion(originalQuestion);
     }
   }, [originalQuestion]);
 
   const classes = useStyles();
   const history = useHistory();
-  const [expanded, setExpanded] = useState(false);
   const [imgPreview, setImgPreview] = useState(Placeholder);
   
   const checkURL = () => {
@@ -182,11 +161,24 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
   };
   
   const onChangeMaker = useCallback((field) => ({ currentTarget }) => { setQuestion({ ...question, [field]: currentTarget.value }); }, [question, setQuestion]);
+  const onSelectMaker = useCallback((field) => ({target}) => { setQuestion({ ...question, [field]: target.value }); }, [question, setQuestion]);
 
   const onStepChangeMaker = useCallback((index) => ({ currentTarget }) => {
     const newInstructions = [...question.instructions];
     newInstructions[index] = currentTarget.value;
     setQuestion({ ...question, instructions: newInstructions});
+  }, [question, setQuestion]);
+
+  const onWrongChoiceChangeMaker = useCallback((wrongAnswersIndex) => ({ currentTarget }) => {
+    const newWrongAnswers = [...question.wrongAnswers];
+    newWrongAnswers[wrongAnswersIndex].choice = currentTarget.value;
+    setQuestion({ ...question, wrongAnswers: newWrongAnswers});
+  }, [question, setQuestion]);
+
+  const onWrongExplanationChangeMaker = useCallback((wrongAnswersIndex) => ({ currentTarget }) => {
+    const newWrongAnswers = [...question.wrongAnswers];
+    newWrongAnswers[wrongAnswersIndex].explanation = currentTarget.value;
+    setQuestion({ ...question, wrongAnswers: newWrongAnswers});
   }, [question, setQuestion]);
 
   const addInstruction = useCallback(() => {
@@ -195,8 +187,26 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
   }, [question, setQuestion]);
 
   const handleSaveQuestion = useCallback(() => {
+    if(question.text == null || question.text == "") {
+      window.alert("Please enter a question");
+      return;
+    }
+    if(question.answer == null || question.answer == "") {
+      window.alert("Please enter an answer");
+      return;
+    }
+    if(question.grade == null || question.grade == "") {
+      window.alert("Please enter a grade level");
+      return;
+    }
+    if(question.domain == null || question.domain == "") {
+      window.alert("Please enter a domain/subject");
+      return;
+    }
     if (question.instructions != null && question.instructions !== []) question.instructions = JSON.stringify(question.instructions);
-    saveQuestion(question, gameId).then(() => history.push(`/games/${gameIndex}`));
+    if (question.wrongAnswers != null && question.wrongAnswers !== []) question.wrongAnswers = JSON.stringify(question.wrongAnswers);
+    // saveQuestion(question, gameId).then(() => history.push(`/games/${gameIndex}`));
+    saveQuestion(question, gameId).then(() => history.push(`/`));
   }, [question, saveQuestion, history, gameId, gameIndex]);
 
   const handleBack = useCallback(() => {
@@ -213,12 +223,11 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
+      <Button type="button" className={classes.back} onClick={handleBack}>
+        <ArrowBack style={{marginRight: 8}} />Back to Game Maker
+      </Button>
       <div style={{position: 'relative', height: 420, marginTop: 30}}>
-        <Typography gutterBottom variant="h4" component="h1">
-          Question
-
-          <Button className={classes.back} onClick={handleBack}>Back</Button>
-        </Typography>
+        <Typography gutterBottom variant="h4" component="h1">Question</Typography>
         
         <TextField className={classes.input} style={{width: 632}} id="question-text" value={question.text} onChange={onChangeMaker('text')} label="Question Text" variant="outlined" multiline rows={12} required />
         <div className={classes.imagePreview}>
@@ -230,42 +239,87 @@ export default function QuestionForm({ loading, saveQuestion, deleteQuestion, qu
 
       <Divider className={classes.divider} />
 
-      <div style={{position: 'relative'}}>
-        <Grid item xs={12}>
-          <Paper className={classes.correctCard}>
-            <CardContent style={{display: 'flex', justifyContent: 'space-between'}}>
-              <Typography className={classes.answer}>
-                  Correct Answer:
-              </Typography>
-              <TextField size="small" style={{width: 577, margin: 0}} id="answer" value={question.answer} onChange={onChangeMaker('answer')} label="Type Answer Here" variant="outlined" required />
-              <IconButton size='small' className={expanded ? classes.expanded : classes.expand} expand={expanded} onClick={() => setExpanded(!expanded)}>
-                <ExpandMore fontSize='large'/>
-              </IconButton>
-            </CardContent>
-
-            <Collapse in={expanded}>
-              <CardContent>
-                <Typography className={classes.explanationTitle}>
-                  Explanation Steps
-                </Typography>
-                <List>
-                  {typeof question.instructions != "string" && question?.instructions?.map((step, index) => (
-                    <React.Fragment key={index}>
-                      <ListItem className={classes.instruction}>
-                        <h1>{index + 1}.</h1>
-                        <TextField className={classes.input} id={`step-${index + 1}`} value={step} onChange={onStepChangeMaker(index)} label="Write text here: Remember to be concise!" size="small" multiline rows={5} variant="outlined" required />
-                        <Button className={classes.deleteButton} onClick={() => handleRemoveInstruction(index)}>X</Button>
-                      </ListItem>
-                    </React.Fragment>
-                  ))}
-                  <ListItem className={classes.instruction}>
-                    <Button className={classes.greenButton} startIcon={<Add style={{width: 28, height: 28}} />} variant="contained" onClick={addInstruction}>Add Step</Button>
-                  </ListItem>
-                </List>
-              </CardContent>
-            </Collapse>
-          </Paper>
-        </Grid>
+      <div>
+        <QuestionFormAnswerDropdown question={question} correct={true} onChangeMaker={onChangeMaker} onStepChangeMaker={onStepChangeMaker} handleRemoveInstruction={handleRemoveInstruction} addInstruction={addInstruction} />
+        <QuestionFormAnswerDropdown question={question} correct={false} onChangeMaker={onChangeMaker} onStepChangeMaker={onStepChangeMaker} handleRemoveInstruction={handleRemoveInstruction} addInstruction={addInstruction} wrongAnswersIndex={0} onWrongChoiceChangeMaker={onWrongChoiceChangeMaker} onWrongExplanationChangeMaker={onWrongExplanationChangeMaker} />
+        <QuestionFormAnswerDropdown question={question} correct={false} onChangeMaker={onChangeMaker} onStepChangeMaker={onStepChangeMaker} handleRemoveInstruction={handleRemoveInstruction} addInstruction={addInstruction} wrongAnswersIndex={1} onWrongChoiceChangeMaker={onWrongChoiceChangeMaker} onWrongExplanationChangeMaker={onWrongExplanationChangeMaker} />
+        <QuestionFormAnswerDropdown question={question} correct={false} onChangeMaker={onChangeMaker} onStepChangeMaker={onStepChangeMaker} handleRemoveInstruction={handleRemoveInstruction} addInstruction={addInstruction} wrongAnswersIndex={2} onWrongChoiceChangeMaker={onWrongChoiceChangeMaker} onWrongExplanationChangeMaker={onWrongExplanationChangeMaker} />
+      </div>
+      
+      <Typography gutterBottom variant="h4" component="h1" style={{marginRight: 30, display: 'inline'}}>Enter CCSS</Typography>
+      <QuestionIcon />
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div className={classes.dropdownWrapper}>
+          <p>Grade Level*</p>
+          <Select 
+            className={classes.dropdown} 
+            label="Grade Level" 
+            onChange={onSelectMaker('grade')}
+            disableUnderline
+            required
+            MenuProps={{classes: {paper: classes.MenuProps}}}
+          >
+            <MenuItem value={"6"}>6</MenuItem>
+            <MenuItem value={"7"}>7</MenuItem>
+            <MenuItem value={"8"}>8</MenuItem>
+            <MenuItem value={"9"}>9</MenuItem>
+            <MenuItem value={"10"}>10</MenuItem>
+            <MenuItem value={"11"}>11</MenuItem>
+            <MenuItem value={"12"}>12</MenuItem>
+          </Select>
+        </div>
+        <div className={classes.dropdownWrapper}>
+          <p>Domain/Subject*</p>
+          <Select 
+            className={classes.dropdown} 
+            label="Domain/Subject" 
+            onChange={onSelectMaker('domain')}
+            disableUnderline
+            required
+            MenuProps={{classes: {paper: classes.MenuProps}}}
+          >
+            <MenuItem value={"RP"}>RP: Ratios & Proportional Relationships</MenuItem>
+            <MenuItem value={"NS"}>NS: Number System</MenuItem>
+            <MenuItem value={"EE"}>EE: Expressions & Equations</MenuItem>
+            <MenuItem value={"G"}>G: Geometry</MenuItem>
+            <MenuItem value={"SP"}>SP: Statistics & Probability</MenuItem>
+            <MenuItem value={"F"}>F: Functions</MenuItem>
+          </Select>
+        </div>
+        <div className={classes.dropdownWrapper}>
+          <p>Cluster</p>
+          <Select 
+            className={classes.dropdown} 
+            label="Cluster" 
+            onChange={onSelectMaker('cluster')}
+            disableUnderline
+            MenuProps={{classes: {paper: classes.MenuProps}}}
+          >
+            <MenuItem value={"A"}>A</MenuItem>
+            <MenuItem value={"B"}>B</MenuItem>
+            <MenuItem value={"C"}>C</MenuItem>
+          </Select>
+        </div>
+        <div className={classes.dropdownWrapper}>
+          <p>Standard</p>
+          <Select 
+            className={classes.dropdown} 
+            label="Standard" 
+            onChange={onSelectMaker('standard')}
+            disableUnderline
+            MenuProps={{classes: {paper: classes.MenuProps}}}
+          >
+            <MenuItem value={"1"}>1</MenuItem>
+            <MenuItem value={"2"}>2</MenuItem>
+            <MenuItem value={"3"}>3</MenuItem>
+            <MenuItem value={"4"}>4</MenuItem>
+            <MenuItem value={"5"}>5</MenuItem>
+            <MenuItem value={"6"}>6</MenuItem>
+            <MenuItem value={"7"}>7</MenuItem>
+            <MenuItem value={"8"}>8</MenuItem>
+            <MenuItem value={"9"}>9</MenuItem>
+          </Select>
+        </div>
       </div>
 
       <div style={{textAlign: 'center', marginTop: 50}}>
