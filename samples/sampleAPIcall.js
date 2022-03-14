38f822a2f3285e114db67cc4dd9d44c1aac9a40c
Ankit Trehan <ankittrehan2000@gmail.com>
2022-03-14 10:23:24 -0400

    Fix API call

--- samples/sampleAPIcall.js
+++ samples/sampleAPIcall.js
@@ -1,8 +1,14 @@
-import { getGameSession } from "./queries";
+import { listGameSessions, getGameSession } from "./queries";
 import { API, graphqlOperation } from 'aws-amplify';
 
-export const getGameSession = async () => {
-  const result = await API.graphql(graphqlOperation(getGameSession));
-  const games = result?.data?.getGameSession || [];
+export const listGS = async () => {
+  const result = await API.graphql(graphqlOperation(listGameSessions));
+  const games = result?.data?.listGameSessions || [];
   return games;
+}
+
+export const getGS = async (id) => {
+  const result = await API.graphql(graphqlOperation(getGameSession, { id }));
+  const game = result?.data?.getGameSession || {};
+  return game;
 }
