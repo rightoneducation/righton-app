af76548b33235d73c271bdad8bbb897d8b70c274
Lucah Endicott <lucahdavid88@gmail.com>
2022-04-12 17:06:30 -0700

    update toggle mode text

--- web/src/components/host/ModeToggle.jsx
+++ web/src/components/host/ModeToggle.jsx
@@ -6,9 +6,9 @@ const ModeToggle = () => {
     const classes = useStyles()
   return (
     <Grid className={classes.modes}>
-        <p style={{cursor: 'pointer'}}>Display Mode</p>
+        <p style={{cursor: 'pointer'}}>Display</p>
             <CachedIcon style={{fontSize: '30px', margin: '20px', marginTop: '9px'}} />
-        <p style={{opacity: '0.3'}}>Host Mode</p>
+        <p style={{opacity: '0.3'}}>Host</p>
      </Grid>
   )
 }
@@ -21,7 +21,8 @@ const useStyles = makeStyles(themes => ({
         color: 'white',
         fontSize: '15.19px',
         textDecoration: 'underline',
-        marginTop: '1px',      
+        marginTop: '1px',
+        fontWeight: 'Bold'      
     },
 }))
 
