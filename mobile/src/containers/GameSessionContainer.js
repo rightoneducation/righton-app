2d14615ba4c1565dc26097999bf14e2df452c8ee
Lucah Endicott <70814349+lucahendicott@users.noreply.github.com>
2022-10-05 14:37:30 -0700

    set initial state values for game session and team data as values from local storage

--- mobile/src/containers/GameSessionContainer.js
+++ mobile/src/containers/GameSessionContainer.js
@@ -2,103 +2,117 @@ import React, { useState, useEffect } from "react"
 import EncryptedStorage from "react-native-encrypted-storage"
 
 const GameSessionContainer = ({ children }) => {
-  const [gameCode, setGameCode] = useState(null)
-  const [teamId, setTeamId] = useState(null)
-  const [gameSession, setGameSession] = useState(null)
-  const [teamMember, setTeamMember] = useState(null)
+    const [gameCode, setGameCode] = useState(null)
+    const [teamId, setTeamId] = useState(null)
+    const [gameSession, setGameSession] = useState(null)
+    const [teamMember, setTeamMember] = useState(null)
 
-  //add a way to save the updates to a session storage
-  //load the session storage when the game session container loads
-  //update initial useState values to load data from local storage
-  //based on intial values - redirect to the current game screen
-  //currentState.screenName
+    //add a way to save the updates to a session storage
+    //load the session storage when the game session container loads
 
-  useEffect(() => {
-    let subscription
+    //based on intial values - redirect to the current game screen
 
-    if (gameCode) {
-      global.apiClient
-        .getGameSessionByCode(gameCode)
-        .then((gameSessionResponse) => {
-          setGameSession(gameSessionResponse)
-          if (gameSessionResponse?.id) {
-            subscription = global.apiClient.subscribeUpdateGameSession(
-              gameSessionResponse.id,
-              (gameSessionSubscriptionResponse) => {
-                setGameSession(gameSessionSubscriptionResponse)
-                //TODO: update the team object everytime the game session is updated
-                //only update the team member if the team member is found
-              }
+    //update initial useState values to load data from local storage
+
+    useEffect(() => {
+        loadLocalGameSession().then((localGameSession) => {
+            if (localGameSession) {
+                console.log("local game session", localGameSession)
+                setGameSession(localGameSession.gameSession)
+                setTeamId(localGameSession.teamId)
+                setTeamMember(localGameSession.teamMember)
+                setGameCode(localGameSession.gameCode)
+            }
+        })
+        if (gameSession) {
+            storeGameSessionLocal()
+        }
+    }, [])
+
+    useEffect(() => {
+        let subscription
+
+        if (gameCode) {
+            global.apiClient
+                .getGameSessionByCode(gameCode)
+                .then((gameSessionResponse) => {
+                    setGameSession(gameSessionResponse)
+                    if (gameSessionResponse?.id) {
+                        subscription =
+                            global.apiClient.subscribeUpdateGameSession(
+                                gameSessionResponse.id,
+                                (gameSessionSubscriptionResponse) => {
+                                    setGameSession(
+                                        gameSessionSubscriptionResponse
+                                    )
+                                    //TODO: update the team object everytime the game session is updated
+                                    //only update the team member if the team member is found
+                                }
+                            )
+                    }
+                })
+        }
+
+        return () => subscription?.unsubscribe()
+    }, [gameCode])
+
+    // useEffect(() => {
+    //     // TODO: this is how you get to the correct page initially
+    //     if (gameSession?.currentState === "CHOOSE_CORRECT_ANSWER") {
+    //         // navigate to pregame countdown screen
+    //         Screen.GamePreview
+    //     }
+    // }, [gameSession?.currentState])
+
+    async function storeGameSessionLocal() {
+        try {
+            const localGameSession = await EncryptedStorage.setItem(
+                "localGameSession",
+                JSON.stringify({
+                    gameSession: gameSession,
+                    teamId: teamId,
+                    teamMember: teamMember,
+                    gameCode: gameCode,
+                    currentState: gameSession?.currentState,
+                })
             )
-          }
-        })
+            console.log("stored game session locally", localGameSession)
+        } catch (error) {
+            // There was an error on the native side
+            console.log("error storing game session", error)
+        }
     }
 
-    return () => subscription?.unsubscribe()
-  }, [gameCode])
-
-  // useEffect(() => {
-  //   // TODO: this is how you get to the correct page initially
-  //   if (gameSession.currentState === "") {
-  //     // navigate somewhere
-  //   }
-  // }, [gameSession?.currentState])
-
-  async function storeGameSessionLocal() {
-    try {
-      const value = await EncryptedStorage.setItem(
-        "localGameSession",
-        JSON.stringify({
-          gameCode: gameCode,
-          teamId: teamId,
-          teamMember: teamMember,
-          gameSession: gameSession,
-        })
-      )
-      console.log("stored game session locally", value)
-    } catch (error) {
-      // There was an error on the native side
-      console.log("error storing game session", error)
+    async function loadLocalGameSession() {
+        try {
+            const value = await EncryptedStorage.getItem("localGameSession")
+            if (value !== null) {
+                // We have data!!
+                console.log("local game session orig", value)
+                return JSON.parse(value)
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log("error loading game session", error)
+        }
     }
-  }
 
-  async function loadGameSessionLocal() {
-    try {
-      const value = await EncryptedStorage.getItem("localGameSession")
-      if (value !== null) {
-        // We have data!!
-        console.log("local game session", value)
-        return value
-      }
-    } catch (error) {
-      // Error retrieving data
-      console.log("error loading game session", error)
+    const setTeamInfo = (team, teamMember) => {
+        setTeamId(team.id)
+        setTeamMember(teamMember)
     }
-  }
 
-  useEffect(() => {
-    loadGameSessionLocal()
-    if (gameSession) {
-      storeGameSessionLocal()
-    }
-  }, [gameSession])
+    console.debug("game session in container:", gameSession)
+    console.debug("team member in container:", teamMember)
+    console.debug("team in container:", teamId)
 
-  console.debug("game session in container:", gameSession)
-
-  const setTeamInfo = (team, teamMember) => {
-    setTeamId(team.id)
-    setTeamMember(teamMember)
-  }
-
-  console.debug("team member in container:", teamMember)
-
-  return children({
-    gameSession,
-    setGameCode,
-    setTeamInfo,
-    teamId,
-    teamMember,
-  })
+    return children({
+        gameSession,
+        setGameCode,
+        setTeamInfo,
+        teamId,
+        teamMember,
+    })
 }
 
 export default GameSessionContainer
