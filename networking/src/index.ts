d0de777231100f2619aa5d1cb5e4c03fb0a07de7
Mani Ramezan <mani.ramezan@gmail.com>
2022-10-23 17:10:04 -0400

    Messed up the path in the prev commit

--- networking/src/index.ts
+++
@@ -1,8 +0,0 @@
-// created from 'create-ts-index'
-
-export * from './AWSMobileApi'
-export * from './ApiClient'
-export * from './IApiClient'
-export * from './ModelHelper'
-export * from './Models'
-export * from './graphql'
