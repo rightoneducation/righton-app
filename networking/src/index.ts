8ef68cac9693d909247bbefccbd5c9d5048fcc96
drewjhart <79417944+drewjhart@users.noreply.github.com>
2022-10-24 08:16:24 -0700

    delete index.ts

--- networking/src/index.ts
+++
@@ -1,8 +0,0 @@
-// created from 'create-ts-index'
-
-export * from './ApiClient'
-export * from './AWSMobileApi'
-export * from './IApiClient'
-export * from './ModelHelper'
-export * from './Models'
-export * from './graphql'
