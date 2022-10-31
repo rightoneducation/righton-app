f294e11d52da409be9dd7d86b4ad0137cc98ea8c
Ryan Booth <ryanbooth77@gmail.com>
2022-10-31 16:44:14 -0700

    spaces

--- mobile/src/Student/screens/Game/PhaseOneBasicGamePlay/AnswerOptionsPhaseOne/index.js
+++ mobile/src/Student/screens/Game/PhaseOneBasicGamePlay/AnswerOptionsPhaseOne/index.js
@@ -7,81 +7,81 @@ import RoundTextIcon from "../../../../components/RoundTextIcon"
 import { KeyboardAwareFlatList } from "@codler/react-native-keyboard-aware-scroll-view"
 
 const AnswerOptionsPhaseOne = ({ onAnswered, answers }) => {
-  const [currentAnswers, setCurrentAnswers] = useState(answers)
+    const [currentAnswers, setCurrentAnswers] = useState(answers)
 
-  const chooseAnswer = (selectedAnswer) => {
-    const answerOptions = currentAnswers.map((answer) => {
-      if (answer.id === selectedAnswer.id) {
-        return {
-          ...answer,
-          isSelected: true,
-        }
-      } else {
-        answer.isSelected = false
-      }
-      return answer
-    })
+    const chooseAnswer = (selectedAnswer) => {
+        const answerOptions = currentAnswers.map((answer) => {
+            if (answer.id === selectedAnswer.id) {
+                return {
+                    ...answer,
+                    isSelected: true,
+                }
+            } else {
+                answer.isSelected = false
+            }
+            return answer
+        })
 
-    setCurrentAnswers(answerOptions)
-    onAnswered(selectedAnswer)
-  }
+        setCurrentAnswers(answerOptions)
+        onAnswered(selectedAnswer)
+    }
 
-  return (
-    <View style={[sharedStyles.cardContainer, styles.container]}>
-      <Text style={[sharedStyles.text, { opacity: 1 }]}>
-        Choose the <Text style={styles.correctAnswerText}>correct answer</Text>
-      </Text>
+    return (
+        <View style={[sharedStyles.cardContainer, styles.container]}>
+            <Text style={[sharedStyles.text, { opacity: 1 }]}>
+                Choose the <Text style={styles.correctAnswerText}>correct answer</Text>
+            </Text>
 
-      <View
-        style={{
-          opacity: 1,
-          flex: 1,
-          alignSelf: "stretch",
-        }}
-      >
-        <KeyboardAwareFlatList
-          style={[styles.answers]}
-          data={currentAnswers}
-          keyExtractor={(item) => `${item.id}`}
-          renderItem={({ item }) => (
-            <RoundTextIcon
-              style={[styles.answerItem]}
-              icon={
-                item.isSelected
-                  ? require("../../img/checkmark_checked.png")
-                  : require("../../img/gray_circle.png")
-              }
-              text={item.text}
-              height={45}
-              borderColor={
-                item.isSelected ? "#159EFA" : "#D9DFE5"
-              }
-              onPress={chooseAnswer}
-              showIcon={item.isSelected}
-              readonly={true}
-              data={item}
-            />
-          )}
-        />
-      </View>
-    </View>
-  )
+            <View
+                style={{
+                    opacity: 1,
+                    flex: 1,
+                    alignSelf: "stretch",
+                }}
+            >
+                <KeyboardAwareFlatList
+                    style={[styles.answers]}
+                    data={currentAnswers}
+                    keyExtractor={(item) => `${item.id}`}
+                    renderItem={({ item }) => (
+                        <RoundTextIcon
+                            style={[styles.answerItem]}
+                            icon={
+                                item.isSelected
+                                    ? require("../../img/checkmark_checked.png")
+                                    : require("../../img/gray_circle.png")
+                            }
+                            text={item.text}
+                            height={45}
+                            borderColor={
+                                item.isSelected ? "#159EFA" : "#D9DFE5"
+                            }
+                            onPress={chooseAnswer}
+                            showIcon={item.isSelected}
+                            readonly={true}
+                            data={item}
+                        />
+                    )}
+                />
+            </View>
+        </View>
+    )
 }
 
 export default AnswerOptionsPhaseOne
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  answers: {
-    marginTop: verticalScale(15),
-    alignSelf: "stretch",
-  },
-  answerItem: {
-  },
-  correctAnswerText: {
-    color: '#349E15'
-  }
+    container: {
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    answers: {
+        marginTop: verticalScale(15),
+        alignSelf: "stretch",
+    },
+    answerItem: {
+    },
+    correctAnswerText: {
+        color: '#349E15'
+    }
 })
