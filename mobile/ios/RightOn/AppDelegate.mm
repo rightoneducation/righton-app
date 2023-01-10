632d2fd3e2e2ff2ddfb1b4770b52760102f7f6fd
Mani Ramezan <mani.ramezan@gmail.com>
2023-01-10 00:49:12 -0500

    [Mobile] [iOS] Fix CodePush import placement

--- mobile/ios/RightOn/AppDelegate.mm
+++ mobile/ios/RightOn/AppDelegate.mm
@@ -6,6 +6,8 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <CodePush/CodePush.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -15,8 +17,6 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
-
-#import <CodePush/CodePush.h>
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
