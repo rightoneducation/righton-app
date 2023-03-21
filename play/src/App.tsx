a3fd8ffa24a39d6f43ae344579ba004d3966ae79
drew <drew@drews-Air.hitronhub.home>
2023-03-21 10:25:23 -0700

    inital setup

---
+++ play/src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import {
+  createTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import GameInProgress from './pages/GameInProgress';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#307583',
+    },
+    secondary: {
+      main: '#8e2e9d',
+    },
+  },
+  typography: {
+    fontFamily: 'Poppins',
+  },
+});
+
+const RedirectToCentralIfMissing = () => {
+  window.location.href = 'http://central.rightoneducation.com/';
+  return null;
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/" >
+            <GameInProgress />
+          </Route>
+          <Route>
+            <RedirectToCentralIfMissing />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ play/src/pages/GameInProgress.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GameInProgress() {
+  return(
+    <div> SUP! </div>
+  )
+}
