61f1fbbc81cb6e878c7fee634b6f3d42c00bb9cd
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-04-18 16:22:52 -0700

    selectavatar button press animation

--- play/src/components/joingame/SelectAvatar.tsx
+++ play/src/components/joingame/SelectAvatar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
-import { Stack, Box, Typography } from '@mui/material';
+import { Stack, Box, Button, Typography } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
 import {
   GamePlayButton,
@@ -24,6 +24,27 @@ const GridContainer = styled('div')(({ theme }) => ({
   spacing: `${theme.sizing.mediumPadding}px`,
 }));
 
+const NewPointsAnimation = styled('div')({
+  animation: `newScoreUp 1000ms cubic-bezier(0.4, 0, 0.2, 1)`,
+  opacity: 0,
+  position: 'absolute',
+  zIndex: 2,
+  '@keyframes newScoreUp': {
+    '0%': {
+      opacity: 0,
+      transform: 'translateY(-110%)',
+    },
+    '50%': {
+      opacity: 1,
+      transform: 'translateY(-110%)',
+    },
+    '100%': {
+      opacity: 1,
+      transform: 'translateY(0)',
+    },
+  },
+});
+
 const IconContainer = styled(Box)({
   // container for monster icons in grid
   height: '118px',
@@ -31,16 +52,39 @@ const IconContainer = styled(Box)({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+  borderRadius: '20px',
 });
 
-const Icon = styled('img')({
+interface IconProps {
+  isSelected: boolean;
+}
+
+const Icon = styled('img', {
+  shouldForwardProp: (prop) => prop !== 'isSelected',
+})<IconProps>(({ isSelected }) => ({
   height: '106px',
   width: 'auto',
   boxShadow: '0px 8px 20px rgba(26, 100, 136, 0.3)',
   borderRadius: '20px',
   borderColor: 'white',
   borderStyle: 'solid',
-});
+  animation: isSelected ? `bounceAnimation 300ms` : 'none',
+  '@keyframes bounceAnimation': {
+    '0%': {
+      opacity: 0,
+      transform: 'scale(1)',
+    },
+    '50%': {
+      opacity: 1,
+      transform: 'scale(0.95)',
+    },
+    '100%': {
+      opacity: 1,
+      transform: 'scale(1.0)',
+    },
+  },
+})
+);
 
 const MonsterContainer = styled(Box)({
   display: 'flex',
@@ -52,6 +96,21 @@ const MonsterContainer = styled(Box)({
 const Monster = styled('img')({
   width: '250px',
   height: 'auto',
+  animation: `bounceAnimation 300ms`,
+  '@keyframes bounceAnimation': {
+    '0%': {
+      opacity: 0,
+      transform: 'scale(1)',
+    },
+    '50%': {
+      opacity: 1,
+      transform: 'scale(0.95)',
+    },
+    '100%': {
+      opacity: 1,
+      transform: 'scale(1.0)',
+    },
+  },
 });
 
 const BottomContainer = styled(Box)({
@@ -77,6 +136,7 @@ export default function SelectAvatar({
   isMobileDevice,
 }: SelectAvatarProps) {
   const theme = useTheme();
+
   return (
     <JoinGameBackgroundContainer>
       <StackContainer>
@@ -98,6 +158,7 @@ export default function SelectAvatar({
                   onClick={() => {
                     handleAvatarSelected(index);
                   }}
+                  isSelected = {index === selectedAvatar}
                   alt="avatar"
                   sx={{ borderWidth: index === selectedAvatar ? '6px' : '0px' }}
                 />
