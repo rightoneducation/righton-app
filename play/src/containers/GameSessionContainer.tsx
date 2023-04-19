23e5cf0ca6606774ff1cbd10fce4dc81d72450d5
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-04-19 09:05:20 -0700

    breaking apart PR into smaller pieces for easier review

--- play/src/containers/GameSessionContainer.tsx
+++ play/src/containers/GameSessionContainer.tsx
@@ -18,29 +18,6 @@ export default function GameSessionContainer() {
   const [teamAvatar, setTeamAvatar] = useState(0); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [joinGameState, setjoinGameState] = useState<JoinGameState>(JoinGameState.SPLASH);
 
-  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Number(event.target.value);
-    switch (value) {
-      case 0:
-        setjoinGameState(JoinGameState.SPLASH);
-        break;
-      case 1:
-        setjoinGameState(JoinGameState.ENTERGAMECODE);
-        break;
-      case 2:
-        setjoinGameState(JoinGameState.ENTERNAME);
-        break;
-      case 3:
-        setjoinGameState(JoinGameState.SELECTAVATAR);
-        break;
-      case 4:
-        setjoinGameState(JoinGameState.HOWTOPLAY);
-        break;
-      default:
-        // Handle invalid value here
-        break;
-    }
-  };
 
   return (
     // <GameInProgress
@@ -48,48 +25,6 @@ export default function GameSessionContainer() {
     //   teamAvatar={teamAvatar}
     //   teamId="2d609343-de50-4830-b65e-71eb72bb9bef"
     // />
-    <>
-      <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 5 }}>
-        <div>
-          <input
-            type="radio"
-            name="option"
-            value="0"
-            checked={joinGameState === JoinGameState.SPLASH}
-            onChange={handleOptionChange}
-          />
-          <input
-            type="radio"
-            name="option"
-            value="1"
-            checked={joinGameState === JoinGameState.ENTERGAMECODE}
-            onChange={handleOptionChange}
-          />
-          <input
-            type="radio"
-            name="option"
-            value="2"
-            checked={joinGameState === JoinGameState.ENTERNAME}
-            onChange={handleOptionChange}
-          />
-          <input
-            type="radio"
-            name="option"
-            value="3"
-            checked={joinGameState === JoinGameState.SELECTAVATAR}
-            onChange={handleOptionChange}
-          />
-          <input
-            type="radio"
-            name="option"
-            value="4"
-            checked={joinGameState === JoinGameState.HOWTOPLAY}
-            onChange={handleOptionChange}
-          />
-        </div>
-      </div>
-
-      <JoinGame joinGameState={joinGameState} />
-    </>
+    <JoinGame joinGameState={joinGameState} />
   );
 }
--- play/src/pages/JoinGame.tsx
+++ play/src/pages/JoinGame.tsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import SplashScreen from '../components/joingame/SplashScreen';
-import EnterGameCode from '../components/joingame/EnterGameCode';
-import EnterPlayerName from '../components/joingame/EnterPlayerName';
-import HowToPlay from '../components/joingame/HowToPlay';
-import SelectAvatar from '../components/joingame/SelectAvatar';
 import { JoinGameState } from '../lib/PlayModels';
 
 interface JoinGameProps {
@@ -23,36 +19,6 @@ export default function JoinGame({ joinGameState }: JoinGameProps) {
   const [selectedAvatar, setSelectedAvatar] = useState<number | null>(null);
 
   switch (joinGameState) {
-    case JoinGameState.ENTERGAMECODE:
-      return (
-        <EnterGameCode
-          gameCodeValue={gameCodeValue}
-          handleGameCodeChange={setGameCodeValue}
-          inputError={inputError}
-        />
-      );
-    case JoinGameState.ENTERNAME:
-      return (
-        <EnterPlayerName
-          playerFirstName={playerFirstName}
-          playerLastName={playerLastName}
-          handlePlayerFirstNameChange={setPlayerFirstName}
-          handlePlayerLastNameChange={setPlayerLastName}
-          inputError={inputError}
-        />
-      );
-    case JoinGameState.SELECTAVATAR:
-      return (
-        <SelectAvatar
-          selectedAvatar={selectedAvatar}
-          handleAvatarSelected={setSelectedAvatar}
-          playerFirstName={playerFirstName}
-          playerLastName={playerLastName}
-          isMobileDevice={isMobileDevice}
-        />
-      );
-    case JoinGameState.HOWTOPLAY:
-      return <HowToPlay />;
     case JoinGameState.SPLASH:
     default:
       return <SplashScreen />;
