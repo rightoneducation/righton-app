6b63b6441e4a8899de3160ed1052b915690322c3
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-04-25 13:32:26 -0700

    fixes per ryan's comments

--- play/src/containers/GameSessionContainer.tsx
+++ play/src/containers/GameSessionContainer.tsx
@@ -20,11 +20,6 @@ export default function GameSessionContainer() {
 
 
   return (
-    // <GameInProgress
-    //   {...gameSession}
-    //   teamAvatar={teamAvatar}
-    //   teamId="2d609343-de50-4830-b65e-71eb72bb9bef"
-    // />
     <JoinGame joinGameState={joinGameState} />
   );
 }
--- play/src/pages/JoinGame.tsx
+++ play/src/pages/JoinGame.tsx
@@ -11,11 +11,9 @@ interface JoinGameProps {
 export default function JoinGame({ joinGameState }: JoinGameProps) {
   const theme = useTheme();
   const isMobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
-  const [inputError, setInputError] = useState(true); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [gameCodeValue, setGameCodeValue] = useState('####');
   const [playerFirstName, setPlayerFirstName] = useState('First Name');
   const [playerLastName, setPlayerLastName] = useState('Last Name');
-  const [avatar, setAvatar] = useState(0); // eslint-disable-line @typescript-eslint/no-unused-vars
   const [selectedAvatar, setSelectedAvatar] = useState<number | null>(null);
 
   switch (joinGameState) {
