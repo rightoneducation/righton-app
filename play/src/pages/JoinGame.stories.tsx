cd3dcd3c83d7411e0e69776ba1cc24324b48a1d0
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-05-12 09:54:17 -0700

    fixes JoinGame story

--- play/src/pages/JoinGame.stories.tsx
+++ play/src/pages/JoinGame.stories.tsx
@@ -20,27 +20,6 @@ const Template: ComponentStory<typeof JoinGame> = function JoinGameTemplate(
   );
 };
 
-export const SplashScreen = Template.bind({});
-SplashScreen.args = {
-  // joinGameState: JoinGameState.SPLASH_SCREEN,
-};
-
-export const EnterGameCode = Template.bind({});
-EnterGameCode.args = {
-  // joinGameState: JoinGameState.ENTER_GAME_CODE,
-};
-
-export const EnterPlayerName = Template.bind({});
-EnterPlayerName.args = {
-  // joinGameState: JoinGameState.ENTER_NAME,
-};
-
-export const SelectAvatar = Template.bind({});
-SelectAvatar.args = {
-  // joinGameState: JoinGameState.SELECT_AVATAR,
-};
-
-export const HowToPlay = Template.bind({});
-HowToPlay.args = {
-  // joinGameState: JoinGameState.HOW_TO_PLAY,
+export const Default = Template.bind({});
+Default.args = {
 };
