d6b1d057dcbdb0553c3976c4b7f47ed9537b7121
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-05-30 17:58:35 -0700

    cleans up GameInProgressContainer

--- play/src/containers/GameInProgressContainer.tsx
+++ play/src/containers/GameInProgressContainer.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
   ApiClient,
+  isNullOrUndefined
 } from '@righton/networking';
+import { redirect } from 'react-router-dom';
 import useFetchAndSubscribeGameSession from '../hooks/useFetchAndSubscribeGameSession';
 import GameSessionSwitch from '../components/GameSessionSwitch';
 import HowToPlay from '../pages/pregame/HowToPlay';
@@ -12,14 +14,21 @@ interface GameInProgressContainerProps {
 
 export default function GameInProgressContainer(props:GameInProgressContainerProps) {
   const { apiClient } = props;
-  const pregameModel = JSON.parse(window.localStorage.getItem('rightOn') ?? '{}');
-  // subscribes to gameSession using useSubscribeGameSession hook (uses useSyncExternalStore)
-  const subscription = useFetchAndSubscribeGameSession(pregameModel.gameSessionId, apiClient);
+  const pregameModel = window.localStorage.getItem('rightOn');
+  // if user doesn't have prexisting game info stored locally (from either pregame or dropped game), redirect to pregame
+  if (isNullOrUndefined(pregameModel))
+    return redirect('/');
 
+  const parsedPregameModel = JSON.parse(pregameModel);
+  // subscribes to gameSession using useSubscribeGameSession hook (uses useSyncExternalStore)
+  const  subscription = useFetchAndSubscribeGameSession(parsedPregameModel.gameSessionId, apiClient);
+
+  // if gamesession is loading or errored
   if (!subscription.gameSession){
     if (subscription.isLoading) 
       return <HowToPlay isLoading isError=''/>;
     return <HowToPlay isLoading={false} isError={subscription.error ?? 'sup'}/>;
   }
-  return <GameSessionSwitch pregameModel={pregameModel} gameSession={subscription.gameSession} {...props}/>
+  // otherwise render GameSessionSwitch (which will initially render HowToPlay without intro or error conditions)
+  return <GameSessionSwitch pregameModel={parsedPregameModel} gameSession={subscription.gameSession} {...props}/>
 }
