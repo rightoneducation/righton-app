7418be772e6205f8b320ab6dfb2768b9a7e11494
AlexandraKushnirsky <ask256@cornell.edu>
2023-06-16 17:11:04 -0400

    Formatted files in git diff

--- play/src/components/AnswerCard.stories.tsx
+++ play/src/components/AnswerCard.stories.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { GameSessionState } from '@righton/networking';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { ThemeProvider } from '@mui/material/styles';
-import AnswerCard from './AnswerCard';
-import Theme from '../lib/Theme';
+import React from "react";
+import { GameSessionState } from "@righton/networking";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ThemeProvider } from "@mui/material/styles";
+import AnswerCard from "./AnswerCard";
+import Theme from "../lib/Theme";
 
 export default {
-  title: 'Design System/3_Organisms/AnswerCard',
+  title: "Design System/3_Organisms/AnswerCard",
   component: AnswerCard,
 } as ComponentMeta<typeof AnswerCard>;
 
@@ -25,10 +25,10 @@ export const CorrectAnswer = Template.bind({});
 CorrectAnswer.args = {
   currentState: GameSessionState.CHOOSE_CORRECT_ANSWER,
   answers: [
-    { text: 'Answer 1', isCorrectAnswer: true },
-    { text: 'Answer 2', isCorrectAnswer: false },
-    { text: 'Answer 3', isCorrectAnswer: false },
-    { text: 'Answer 4', isCorrectAnswer: false },
+    { text: "Answer 1", isCorrectAnswer: true },
+    { text: "Answer 2", isCorrectAnswer: false },
+    { text: "Answer 3", isCorrectAnswer: false },
+    { text: "Answer 4", isCorrectAnswer: false },
   ],
 };
 
@@ -36,9 +36,9 @@ export const TrickAnswer = Template.bind({});
 TrickAnswer.args = {
   currentState: GameSessionState.CHOOSE_TRICKIEST_ANSWER,
   answers: [
-    { text: 'Answer 1', isCorrectAnswer: true },
-    { text: 'Answer 2', isCorrectAnswer: false },
-    { text: 'Answer 3', isCorrectAnswer: false },
-    { text: 'Answer 4', isCorrectAnswer: false },
+    { text: "Answer 1", isCorrectAnswer: true },
+    { text: "Answer 2", isCorrectAnswer: false },
+    { text: "Answer 3", isCorrectAnswer: false },
+    { text: "Answer 4", isCorrectAnswer: false },
   ],
 };
--- play/src/components/AnswerCard.tsx
+++ play/src/components/AnswerCard.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { Typography, Stack, Box } from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { isNullOrUndefined, GameSessionState } from '@righton/networking';
-import AnswerSelector from './AnswerSelector';
-import ButtonSubmitAnswer from './ButtonSubmitAnswer';
-import { AnswerState } from '../lib/PlayModels';
-import BodyCardStyled from '../lib/styledcomponents/BodyCardStyled';
-import BodyCardContainerStyled from '../lib/styledcomponents/BodyCardContainerStyled';
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import { Typography, Stack, Box } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { isNullOrUndefined, GameSessionState } from "@righton/networking";
+import AnswerSelector from "./AnswerSelector";
+import ButtonSubmitAnswer from "./ButtonSubmitAnswer";
+import { AnswerState } from "../lib/PlayModels";
+import BodyCardStyled from "../lib/styledcomponents/BodyCardStyled";
+import BodyCardContainerStyled from "../lib/styledcomponents/BodyCardContainerStyled";
 
 interface AnswerCardProps {
   answers: { text: string; isCorrectAnswer: boolean }[] | undefined;
@@ -29,33 +29,33 @@ export default function AnswerCard({
   const theme = useTheme();
   const { t } = useTranslation();
   const correctText = (
-    <Box display="inline" sx={{ textAlign: 'center' }}>
+    <Box display="inline" sx={{ textAlign: "center" }}>
       <Typography variant="h4" display="inline">
-        {t('gameinprogress.chooseanswer.correcttext1')}
+        {t("gameinprogress.chooseanswer.correcttext1")}
       </Typography>
       <Typography
         variant="h4"
         display="inline"
         sx={{ color: `${theme.palette.primary.green}` }}
       >
-        &nbsp;{t('gameinprogress.chooseanswer.correcttext2')}
+        &nbsp;{t("gameinprogress.chooseanswer.correcttext2")}
       </Typography>
     </Box>
   );
   const trickText = (
-    <Box display="inline" sx={{ textAlign: 'center' }}>
+    <Box display="inline" sx={{ textAlign: "center" }}>
       <Typography variant="h4" display="inline">
-        {t('gameinprogress.chooseanswer.incorrecttext1')}&nbsp;
+        {t("gameinprogress.chooseanswer.incorrecttext1")}&nbsp;
       </Typography>
       <Typography
         display="inline"
         variant="h4"
         sx={{ color: `${theme.palette.primary.red}` }}
       >
-        {t('gameinprogress.chooseanswer.incorrecttext2')}&nbsp;
+        {t("gameinprogress.chooseanswer.incorrecttext2")}&nbsp;
       </Typography>
       <Typography variant="h4" display="inline">
-        {t('gameinprogress.chooseanswer.incorrecttext3')}
+        {t("gameinprogress.chooseanswer.incorrecttext3")}
       </Typography>
     </Box>
   );
@@ -78,7 +78,7 @@ export default function AnswerCard({
         {currentState === GameSessionState.CHOOSE_CORRECT_ANSWER
           ? correctText
           : trickText}
-        <Stack spacing={2} sx={{ width: '100%' }}>
+        <Stack spacing={2} sx={{ width: "100%" }}>
           {answers?.map((answer, index) => (
             <AnswerSelector
               answerStatus={getAnswerStatus(answer, index)}
