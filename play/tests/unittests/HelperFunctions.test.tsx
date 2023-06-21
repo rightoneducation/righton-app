50df40c17f20900abf4a07208bbfa6390bcbe94f
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-06-21 13:03:24 -0700

    helper functions tests 1/2

---
+++ play/tests/unittests/HelperFunctions.test.tsx
@@ -0,0 +1,32 @@
+import {
+  isNameValid,
+  isGameCodeValid,
+  checkForSubmittedAnswerOnRejoin,
+  fetchLocalData
+} from '../../src/lib/HelperFunctions';
+import { InputPlaceholder } from '../../src/lib/PlayModels';
+import i18n from '../mockTranslations';
+
+describe('HelperFunctions', () => {
+  it ('isNameValid', () => {
+    const placeholderFirstName = i18n.t('joingame.playername.firstnamedefault');
+    const placeholderLastName = i18n.t('joingame.playername.lastnamedefault');
+    // expects empty or placeholder names to be invalid, all else valid
+    expect(isNameValid('a')).toBe(true);
+    expect(isNameValid('')).toBe(false);
+    expect(isNameValid(placeholderFirstName)).toBe(false);
+    expect(isNameValid(placeholderLastName)).toBe(false);
+  });
+
+  it ('isGameCodeValid', () => {
+    // expects 4 digit numerical game code to be valid, all else invalid
+    const placeholderGameCode = InputPlaceholder.GAME_CODE;
+    expect(isGameCodeValid('1234')).toBe(true);
+    expect(isGameCodeValid('')).toBe(false);
+    expect(isGameCodeValid('a')).toBe(false);
+    expect(isGameCodeValid('123')).toBe(false);
+    expect(isGameCodeValid('12345')).toBe(false);
+    expect(isGameCodeValid(placeholderGameCode)).toBe(false);
+  });
+
+});
