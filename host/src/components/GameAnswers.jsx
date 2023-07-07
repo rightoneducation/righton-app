ba575d12a95b0b0417b490b0c58e791b070b7ed8
josephinaim <jhi002247@gmail.com>
2023-07-07 18:26:03 -0400

    styles added

--- host/src/components/GameAnswers.jsx
+++ host/src/components/GameAnswers.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import GameAnswersDropdown from "./GameAnswersDropdown";
 import { isNullOrUndefined } from "@righton/networking";
 
-export default function GameAnswers({ questions, questionChoices, currentQuestionIndex, answersByQuestion, totalAnswers}) {
+export default function GameAnswers({ questions, questionChoices, currentQuestionIndex, answersByQuestion, totalAnswers }) {
   const classes = useStyles();
   let instructions = "";
   // returns the correct answer explanation for an individual question
   const getAnswerExplanation = (questions, currentQuestionIndex) => {
     if (isNullOrUndefined(questions) || questions.length <= currentQuestionIndex || isNullOrUndefined(questions[currentQuestionIndex].instructions)) {
-        return null;
+      return null;
     }
     questions[currentQuestionIndex].instructions.map((step, index) => {
-    instructions += "Step " + (index+1) + ": " + step + "\n\n";
+      instructions += "Step " + (index + 1) + ": " + step + "\n\n";
     });
     return instructions;
   };
@@ -22,11 +22,16 @@ export default function GameAnswers({ questions, questionChoices, currentQuestio
 
   return (
     <Grid className={classes.background}>
-     {(questionChoices) ? 
-      questionChoices.map((choice,index) => {
-        return (<GameAnswersDropdown key={index}  answer={choice.text} explanation={!choice.isAnswer ? choice.reason : answerExplanation} correct={choice.isAnswer} numQuestionAnswers={answersByQuestion[index]} totalAnswers={totalAnswers} pos ={index}/>)
-      })
-      : null} 
+      <Grid container className={classes.centerContent}>
+        <Grid container>
+          <Typography className={classes.titleStyle}>Answer Explainations</Typography>
+        </Grid>
+      </Grid>
+      {(questionChoices) ?
+        questionChoices.map((choice, index) => {
+          return (<GameAnswersDropdown key={index} answer={choice.text} explanation={!choice.isAnswer ? choice.reason : answerExplanation} correct={choice.isAnswer} numQuestionAnswers={answersByQuestion[index]} totalAnswers={totalAnswers} pos={index} />)
+        })
+        : null}
     </Grid>
   );
 }
@@ -43,5 +48,21 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     fontSize: '24px',
     marginBottom: '10px',
-  }
+  },
+  titleStyle: {
+    color: "var(--teacher-element-foreground, #FFF)",
+    fontFamily: "Poppins",
+    fontSize: "24px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+    textTransform: "none",
+  },
+  centerContent: {
+    display: "flex",
+    justifyContent: "center",
+    width: '95%',
+    margin: 'auto',
+    padding: "20px 40px",
+  },
 }));
---
+++ host/src/components/Responses.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Grid,
+    Typography,
+  } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  titleStyle: {
+    color: "var(--teacher-element-foreground, #FFF)",
+    fontFamily: "Poppins",
+    fontSize: "24px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+    textTransform: "none", 
+  },
+  centerContent: {
+    display: "flex",
+    justifyContent: "center",
+    width: '95%',
+    margin: 'auto',
+    padding: "20px 40px",
+  },
+});
+
+export default function Responses() {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.centerContent}>
+        <Grid container className={classes.titleContainer}>
+            <Typography className={classes.titleStyle}>Real-time Responses</Typography>
+        </Grid>
+    </Grid>
+  );
+}
