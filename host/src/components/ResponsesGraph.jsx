bec13c96cc003c6e8870da6b17b658c56dba9f33
josephinaim <jhi002247@gmail.com>
2023-07-08 23:21:13 -0400

    major styling done, need to fix hard coded positioning

--- host/src/components/ResponsesGraph.jsx
+++ host/src/components/ResponsesGraph.jsx
@@ -1,63 +1,129 @@
 import React from 'react';
 import { LinearProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    progressBar: {
+        backgroundColor: 'white',
+        borderRadius: '0 5px 5px 0',
+        '& .MuiLinearProgress-bar': {
+            borderRadius: '0 5px 5px 0',
+        },
+    },
+    firstProgressBar: {
+        backgroundColor: 'transparent',
+        borderRadius: '5px',
+        border: '1px solid white',
+        '& .MuiLinearProgress-bar': {
+            borderRadius: '5px',
+        },
+    },
+    countTextStyle: {
+        color: 'white',
+    },
+});
 
 const ResponsesGraph = ({ responses }) => {
-  const maxCount = Math.max(...responses.map(response => response.count));
-  const numLines = Math.ceil(maxCount / 5); // Calculate the number of lines needed
+    const maxCount = Math.max(...responses.map(response => response.count));
+    const numLines = Math.ceil(maxCount / 5);
+    const classes = useStyles();
 
-  return (
-    <div style={{ position: 'relative', width: '100%', textAlign: 'center' }}>
-      <div style={{ fontWeight: 'bold', marginBottom: '10px',color: 'white', }}>Number of Players</div>
-      {responses.map(response => {
-        const barLength = (response.count / maxCount) * 100;
-        return (
-          <div key={response.label} style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{color: 'white', }}>{response.label}</div>
-            <div style={{ position: 'relative', width: '100%' }}>
-              <LinearProgress
-                variant="determinate"
-                value={barLength}
-                style={{ height: '20px', borderRadius: '5px' }}
-              />
-              <div
-                style={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: `${barLength}%`,
-                  transform: 'translate(-50%, -50%)',
-                  fontWeight: 'bold',
-                  fontSize: '12px',
-                  color: 'white',
-                  zIndex: 1,
-                }}
-              >
-                {response.count}
-              </div>
-            </div>
-          </div>
-        );
-      })}
-      {/* Render the vertical lines */}
-      {Array.from({ length: numLines + 1 }).map((_, index) => {
-        const position = (index * (100 / numLines));
-        const isCountMultipleOfFive = index !== 0 && (index * 5) % maxCount === 0;
-        return (
-          <div
-            key={index}
-            style={{
-              position: 'absolute',
-              top: '0',
-              bottom: '0',
-              left: `${position}%`,
-              transform: 'translateX(-50%)',
-              borderLeft: '1px solid white',
-              ...(isCountMultipleOfFive && { height: '100%' }),
-            }}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div style={{ position: 'relative', width: '100%', marginLeft: '3%', marginTop: '3%', textAlign: 'center', fontFamily: 'Rubik, sans-serif' }}>
+            <div style={{ fontWeight: 'bold', marginBottom: '10px', color: 'rgba(255, 255, 255, 0.5)' }}>Number of Players</div>
+            {responses.map((response, index) => {
+                const barLength = (response.count / maxCount) * 100;
+                const progressBarClass = index === 0 ? classes.firstProgressBar : classes.progressBar;
+                const countTextStyle = index === 0 ? classes.countTextStyle : ''; 
+
+                return (
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <div style={{ position: 'relative', width: '100%' }}>
+                            <LinearProgress
+                                variant="determinate"
+                                value={barLength}
+                                classes={{
+                                    bar: progressBarClass,
+                                }}
+                                style={{
+                                    height: '20px',
+                                    marginBottom: '5px',
+                                    backgroundColor: 'transparent',
+                                }}
+                            />
+                            <div
+                                style={{
+                                    position: 'absolute',
+                                    top: '40%',
+                                    left: `${barLength - 1}%`,
+                                    transform: 'translate(-50%, -50%)',
+                                    fontWeight: 'bold',
+                                    fontSize: '12px',
+                                    zIndex: 1,
+                                }}
+                                className={countTextStyle}
+                            >
+                                {response.count}
+                            </div>
+                            <div
+                                style={{
+                                    color: 'rgba(255, 255, 255, 0.5)',
+                                    position: 'absolute',
+                                    top: '0%',
+                                    left: '-2%',
+                                    transform: 'translate(-50%, 0)',
+                                    width: '100%',
+                                    textAlign: 'center',
+                                    fontSize: '16px',
+                                    fontWeight: '800',
+                                    fontFamily: 'Poppins'
+                                }}
+                            >
+                                {response.label}
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+            {/* Render the vertical lines */}
+            {Array.from({ length: numLines + 1 }).map((_, index) => {
+                const leftPos = (index * (100 / numLines));
+                return (
+                    <div
+                        style={{
+                            position: 'absolute',
+                            top: '20px',
+                            bottom: '10px',
+                            left: `${leftPos}%`,
+                            transform: 'translateX(-50%)',
+                            borderLeft: '1px solid rgba(255, 255, 255, 0.5)',
+                        }}
+                    >
+                        <div
+                            style={{
+                                position: 'absolute',
+                                top: '-15px',
+                                left: '0',
+                                transform: 'translateX(-50%)',
+                                color: 'rgba(255, 255, 255, 0.5)',
+                                fontSize: '12px',
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {index * 5}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default ResponsesGraph;
+
+
+
+
+
+
+
