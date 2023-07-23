cd28d411fc85ed66e6d82470a91a9e0bc38ece0c
josephinaim <jhi002247@gmail.com>
2023-07-22 21:19:49 -0400

    checkmark implemented for correct answers

--- host/src/components/ResponsesGraph.jsx
+++ host/src/components/ResponsesGraph.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Grid, Typography } from '@mui/material';
 import { VictoryChart, VictoryAxis, VictoryBar, VictoryLabel, VictoryContainer, VictoryPortal } from 'victory';
 import { makeStyles } from '@material-ui/core';
+import check from '../images/Pickedcheck.svg';
 
 const useStyles = makeStyles({
   container: {
@@ -48,11 +49,10 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
     answerCount: response.count,
   }));
 
-  const correctChoiceIndex = questionChoices.findIndex(choice => choice.isAnswer)+1;
+  const correctChoiceIndex = questionChoices.findIndex(choice => choice.isAnswer) + 1;
   //dependent on feedback
   //const largestAnswerCount = Math.max(...data.map(response => response.answerCount));
 
-  console.log(statePosition);
 
   const customTheme = {
     axis: {
@@ -60,10 +60,7 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
         axis: { stroke: 'rgba(255, 255, 255, 0.5)' },
         grid: { stroke: 'transparent' },
         tickLabels: {
-          fill: ( statePosition === 6 ? ({ datum, index }) => (index === reversedResponses.length-1 - correctChoiceIndex ? 'rgba(255, 255, 255, 0.25)' : 'rgba(255, 255, 255, 0.5)') : 'rgba(255, 255, 255, 0.5)'),
-          fontFamily: 'Poppins',
-          fontWeight: '800',
-          padding: 10,
+          padding: 20,
         },
       },
     },
@@ -90,8 +87,6 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
     },
   };
 
-  const axisTickValues = reversedResponses.map(response => response.label);
-
   const [selectedBarInfo, setSelectedBarInfo] = useState(null);
 
   const handleBarClick = (event) => {
@@ -100,6 +95,25 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
     setSelectedBarInfo({ x, y, width, height });
   };
 
+  const CustomTick = ({ x, y, index, text }) => {
+    const showCustomTick = index === reversedResponses.length - 1 - correctChoiceIndex;
+    const fillTick = statePosition === 6 && showCustomTick;
+
+    return (
+      <g>
+        {showCustomTick && (
+          <foreignObject x={x - 25} y={y - 9.5} width={16} height={18}>
+            <img src={check} alt={"correct answer"} />
+          </foreignObject>
+        )}
+        <VictoryLabel x={x} y={y} text={text} style={{
+          fill: fillTick ? 'rgba(255, 255, 255, 0.25)' : 'rgba(255, 255, 255, 0.5)',
+          fontFamily: 'Poppins',
+          fontWeight: '800',
+        }} />
+      </g>
+    );
+  };
 
   return (
     <Grid item xs={12} className={classes.container}>
@@ -113,24 +127,24 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
       >
         <VictoryAxis
           standalone={false}
-          tickValues={axisTickValues}
+          tickLabelComponent={<CustomTick />}
         />
-        {numPlayers < 5 && 
-        <VictoryAxis
-          dependentAxis
-          crossAxis={false}
-          standalone={false}
-          orientation="top"
-          tickValues={[0]}
-        />}
+        {numPlayers < 5 &&
+          <VictoryAxis
+            dependentAxis
+            crossAxis={false}
+            standalone={false}
+            orientation="top"
+            tickValues={[0]}
+          />}
 
-        {numPlayers >= 5 && 
-        <VictoryAxis
-          dependentAxis
-          crossAxis={false}
-          standalone={false}
-          orientation="top"
-        />}
+        {numPlayers >= 5 &&
+          <VictoryAxis
+            dependentAxis
+            crossAxis={false}
+            standalone={false}
+            orientation="top"
+          />}
 
         <VictoryBar
           data={data}
@@ -145,21 +159,21 @@ const ResponsesGraph = ({ studentResponses, numPlayers, totalAnswers, currentSta
               target: 'data',
               eventHandlers: {
                 onClick: (event) => {
-                  handleBarClick(event); 
+                  handleBarClick(event);
                   return [];
                 },
               },
             },
           ]}
         />
-        {selectedBarInfo && ( 
-        <VictoryPortal>
-          <SelectedBar
-            x={selectedBarInfo.x - 20}
-            y={selectedBarInfo.y}
-            width={selectedBarInfo.width + 20}
-            height={selectedBarInfo.height}
-          />
+        {selectedBarInfo && (
+          <VictoryPortal>
+            <SelectedBar
+              x={selectedBarInfo.x - 50}
+              y={selectedBarInfo.y}
+              width={selectedBarInfo.width + 50}
+              height={selectedBarInfo.height}
+            />
           </VictoryPortal>
         )}
       </VictoryChart>
