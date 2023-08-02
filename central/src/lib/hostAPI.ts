9f9cb69f168d8f9b9a15d033e29cb0d449c71ad7
Mani Ramezan <mani.ramezan@gmail.com>
2023-08-01 23:00:04 -0400

    [Central] Formatting

--- central/src/lib/hostAPI.ts
+++ central/src/lib/hostAPI.ts
@@ -1,16 +1,21 @@
-import mockGameSession from '../mocks/gamesession.json'
+import mockGameSession from "../mocks/gamesession.json";
 
 export const loadGameSession = async (gameSessionId: any) => {
-  const gameSession = await Promise.resolve(mockGameSession)
-  return gameSession
-}
+  const gameSession = await Promise.resolve(mockGameSession);
+  return gameSession;
+};
 
 export const removeTeam = async (teamId: number, gameSession: any) => {
-  const updatedGameSession = {...gameSession,
+  const updatedGameSession = {
+    ...gameSession,
     teams: {
-      items: gameSession.teams.items.filter((team: { id: number }) => team.id !== teamId),}}
-  return updatedGameSession
-}
+      items: gameSession.teams.items.filter(
+        (team: { id: number }) => team.id !== teamId
+      ),
+    },
+  };
+  return updatedGameSession;
+};
 
 export const changeGameStatus = async (currentState: any, gameSession: any) => {
   const gameStatuses = [
@@ -18,7 +23,15 @@ export const changeGameStatus = async (currentState: any, gameSession: any) => {
     "REVIEWING_RESULT",
     "CHOOSING_TRICK_ANSWER",
     "FINISHED",
-  ]
-  const updatedGameSession = {...gameSession, currentState: gameStatuses[(gameStatuses.indexOf(currentState) !== (gameStatuses.length-1) ? gameStatuses.indexOf(currentState)+1 : 0)]}
-  return updatedGameSession
-}
+  ];
+  const updatedGameSession = {
+    ...gameSession,
+    currentState:
+      gameStatuses[
+        gameStatuses.indexOf(currentState) !== gameStatuses.length - 1
+          ? gameStatuses.indexOf(currentState) + 1
+          : 0
+      ],
+  };
+  return updatedGameSession;
+};
--- central/src/lib/sorting.ts
+++ central/src/lib/sorting.ts
@@ -1,4 +1,4 @@
-import { Game } from '../API'
+import { Game } from "../API";
 
 export enum SORT_TYPES {
   ALPHABETICAL,
@@ -9,8 +9,8 @@ export enum SORT_TYPES {
   OLDEST,
   REVERSEALPHABETICAL,
   UPDATED,
-};
-  
+}
+
 const sortByUpdated = (a: Game | null, b: Game | null) => {
   if (!a || a.updatedAt === null) return 1;
   if (!b || b.updatedAt === null) return -1;
@@ -35,14 +35,14 @@ const sortByReverseAlphabetical = (a: Game | null, b: Game | null) => {
   return sortAlphabetically(a, b) * -1;
 };
 
-const sortByQuestionAscending = (a: Game | null, b: Game | null) => {  
+const sortByQuestionAscending = (a: Game | null, b: Game | null) => {
   // @ts-ignore: Object is possibly 'null'.
-  if(!a || a.questions === null) return -1;
-  if(!b || b.questions === null) return 1;
+  if (!a || a.questions === null) return -1;
+  if (!b || b.questions === null) return 1;
   // @ts-ignore: Object is possibly 'undefined'.
-  if(a.questions.length > b.questions.length) return 1;
+  if (a.questions.length > b.questions.length) return 1;
   // @ts-ignore: Object is possibly 'undefined'.
-  if(b.questions.length > a.questions.length) return -1;
+  if (b.questions.length > a.questions.length) return -1;
   return 0;
 };
 
@@ -51,12 +51,12 @@ const sortByQuestionDescending = (a: Game | null, b: Game | null) => {
 };
 
 const sortByGradeAscending = (a: Game | null, b: Game | null) => {
-  if(!a || a.grade === null) return -1;
-  if(!b || b.grade === null) return 1;
+  if (!a || a.grade === null) return -1;
+  if (!b || b.grade === null) return 1;
   // @ts-ignore: Object is possibly 'undefined'.
-  if(a.grade > b.grade) return 1;
+  if (a.grade > b.grade) return 1;
   // @ts-ignore: Object is possibly 'undefined'.
-  if(b.grade > a.grade) return -1;
+  if (b.grade > a.grade) return -1;
   return 0;
 };
 
@@ -75,6 +75,9 @@ const SORT_TYPE_TO_FUNCTION = {
   [SORT_TYPES.GRADEDESCENDING]: sortByGradeDescending,
 };
 
-export const sortGamesBySortType = (games: Array<Game | null>, sortType: SORT_TYPES): Game[] => {
+export const sortGamesBySortType = (
+  games: Array<Game | null>,
+  sortType: SORT_TYPES
+): Game[] => {
   return [...games].sort(SORT_TYPE_TO_FUNCTION[sortType]) as Game[];
 };
