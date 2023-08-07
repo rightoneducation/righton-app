f287af26d1e5d57e03f71fbb385e8a096fc43891
AlexandraKushnirsky <ask256@cornell.edu>
2023-08-07 00:21:01 -0400

    styled x axis of bar graph

--- host/src/components/ConfidenceResponseGraph.jsx
+++ host/src/components/ConfidenceResponseGraph.jsx
@@ -9,15 +9,24 @@ const useStyles = makeStyles({
     padding: "5px 4px",
     flexDirection: "column",
     alignItems: "center",
-    alignSelf: "stretch"
+    alignSelf: "stretch",
+    height: 400
   },
-  title: {
+  labels: {
     color: 'rgba(255, 255, 255, 1)',
     fontFamily: 'Rubik',
     fontSize: 12,
     flexDirection: "column",
     alignItems: "center",
     alignSelf: "stretch"
+  },
+  legendContainer: {
+    display: "flex",
+    padding: "5px 4px",
+    flexDirection: "column",
+    alignItems: "center",
+    alignSelf: "stretch",
+    height: 100
   }
 });
 const classes = useStyles;
@@ -26,18 +35,43 @@ const classes = useStyles;
 const ResponsesGraph = () => {
   const correctColor = "#FFF";
   const incorrectColor = "transparent";
+  const customTheme = {
+    axis: {
+      style: {
+        axis: { stroke: 'rgba(255, 255, 255, 0.2)', strokeWidth: 2 },
+        grid: { stroke: 'transparent' },
+        tickLabels: {
+          padding: 10,
+          fill: 'rgba(255, 255, 255, 0.5)',
+          fontSize: 18
+        },
+      },
+    },
+    stack: {
+      colorScale: [incorrectColor, correctColor],
+      style: {
+        data: {
+          stroke: '#FFF',
+          strokeWidth: 2
+        }
+      }
+    },
+    bar: {
+      barWidth: 55
+    }
+  }
 
   const correctResponders = [
-    { x: "Not rated", y: 2 },
-    { x: "Not at all", y: 0 },
+    { x: "Not\nrated", y: 2 },
+    { x: "Not at\nall", y: 0 },
     { x: "Kinda", y: 0 },
     { x: "Quite", y: 2 },
     { x: "Very", y: 6 },
     { x: "Totally", y: 0 }
   ];
   const incorrectResponders = [
-    { x: "Not rated", y: 2 },
-    { x: "Not at all", y: 0 },
+    { x: "Not\nrated", y: 2 },
+    { x: "Not at\nall", y: 0 },
     { x: "Kinda", y: 2 },
     { x: "Quite", y: 4 },
     { x: "Very", y: 4 },
@@ -48,11 +82,11 @@ const ResponsesGraph = () => {
       <div style={{
         display: 'flex',
         justifyContent: 'center',
-        marginBottom: '-3%',
+        marginBottom: '-5%',
         marginTop: '5%'
       }}>
         <Typography
-          className={classes.title}
+          className={classes.labels}
           style={{
             color: 'rgba(255, 255, 255, 1)',
             fontSize: 12,
@@ -61,42 +95,47 @@ const ResponsesGraph = () => {
           Number of players
         </Typography>
       </div>
-      <VictoryStack
-        colorScale={[incorrectColor, correctColor]}
-        domainPadding={0}
-        style={{
-          data: {
-            strokeWidth: 2,
-            stroke: "#FFF"
-          }
-        }}
-        height={190}
-        labelComponent={
-          <VictoryLabel
-            className={classes.title}
-            style={{
-              fill: '#FFF',
-              fontSize: 16
-            }}
-          />
-        }
-      >
-        <VictoryBar
-          data={incorrectResponders}
-          barWidth={55}
-          cornerRadius={({ index }) => correctResponders[index].y === 0 ? 5 : 0}
-        />
-        <VictoryBar
-          data={correctResponders}
-          barWidth={55}
-          cornerRadius={5}
-          labels={({ datum, index }) => datum.y + incorrectResponders[index].y}
-        />
-        <VictoryAxis
-          tickValues={correctResponders.map(el => el.x)}
-        />
+      <VictoryChart theme={customTheme} height={200}>
+        <VictoryStack
+          labelComponent={
+            <VictoryLabel
+              className={classes.labels}
+              style={{
+                fill: '#FFF',
+                fontSize: 18,
 
-      </VictoryStack>
+              }}
+            />
+          }
+        >
+          <VictoryBar
+            data={incorrectResponders}
+            cornerRadius={({ index }) => correctResponders[index].y === 0 ? 5 : 0}
+          />
+          <VictoryBar
+            data={correctResponders}
+            cornerRadius={5}
+            labels={({ datum, index }) => datum.y + incorrectResponders[index].y}
+          />
+          <VictoryAxis
+            tickValues={correctResponders.map(datum => datum.x)}
+          />
+        </VictoryStack>
+      </VictoryChart>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        <Typography
+          className={classes.labels}
+          style={{
+            color: 'rgba(255, 255, 255, 1)',
+            fontSize: 12,
+            opacity: 0.4
+          }}>
+          Confidence
+        </Typography>
+      </div>
     </div>
   );
 };
