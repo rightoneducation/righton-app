7464f4eab2b5bfbfbc26421e6c7323ef86dffcc2
AlexandraKushnirsky <ask256@cornell.edu>
2023-08-07 16:16:54 -0400

    Completed legend on graph, will revisit to improve code quality, reduce redundancy

--- host/src/components/ConfidenceResponseGraph.jsx
+++ host/src/components/ConfidenceResponseGraph.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Typography } from '@material-ui/core';
-import { VictoryChart, VictoryStack, VictoryBar, VictoryLabel, VictoryAxis, VictoryPortal } from 'victory';
+import { Typography, Box } from '@material-ui/core';
+import { VictoryChart, VictoryStack, VictoryBar, VictoryLabel, VictoryAxis, VictoryLegend, Rect } from 'victory';
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -20,13 +20,9 @@ const useStyles = makeStyles({
     alignItems: "center",
     alignSelf: "stretch"
   },
-  legendContainer: {
+  legend: {
     display: "flex",
-    padding: "5px 4px",
-    flexDirection: "column",
-    alignItems: "center",
-    alignSelf: "stretch",
-    height: 100
+    justifyContent: "center"
   }
 });
 const classes = useStyles;
@@ -35,7 +31,7 @@ const classes = useStyles;
 const ResponsesGraph = () => {
   const correctColor = "#FFF";
   const incorrectColor = "transparent";
-  const customTheme = {
+  const customThemeGraph = {
     axis: {
       style: {
         axis: { stroke: 'rgba(255, 255, 255, 0.2)', strokeWidth: 2 },
@@ -60,6 +56,72 @@ const ResponsesGraph = () => {
       barWidth: 55
     }
   }
+
+  const legend =
+    <div className={classes.legend} style={{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: "16px"
+    }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        marginRight: "12px",
+        alignItems: "center"
+      }}>
+        <svg width={40} height={15}>
+          <rect width={40} height={15}
+            style={{
+              fill: "#FFF",
+              stroke: "#FFF",
+              strokeWidth: 2
+            }}
+          />
+        </svg>
+
+        <Typography
+          className={classes.labels}
+          style={{
+            color: 'rgba(255, 255, 255, 1)',
+            fontSize: 12,
+            opacity: 0.4,
+            paddingLeft: 5
+          }}>
+          Correct
+        </Typography>
+      </div>
+
+      <div style={{
+        display: "flex",
+        flexDirection: "row",
+        marginRight: "12px",
+        alignItems: "center"
+      }}>
+        <svg width={40} height={15}>
+          <rect width={40} height={15}
+            style={{
+              fill: "transparent",
+              stroke: "#FFF",
+              strokeWidth: 2
+            }}
+          />
+        </svg>
+
+        <Typography
+          className={classes.labels}
+          style={{
+            color: 'rgba(255, 255, 255, 1)',
+            fontSize: 12,
+            opacity: 0.4,
+            paddingLeft: 5
+          }}>
+          Incorrect
+        </Typography>
+      </div>
+    </div>
+
 
   const correctResponders = [
     { x: "Not\nrated", y: 2 },
@@ -95,7 +157,7 @@ const ResponsesGraph = () => {
           Number of players
         </Typography>
       </div>
-      <VictoryChart theme={customTheme} height={200}>
+      <VictoryChart theme={customThemeGraph} height={200}>
         <VictoryStack
           labelComponent={
             <VictoryLabel
@@ -103,7 +165,6 @@ const ResponsesGraph = () => {
               style={{
                 fill: '#FFF',
                 fontSize: 18,
-
               }}
             />
           }
@@ -136,6 +197,7 @@ const ResponsesGraph = () => {
           Confidence
         </Typography>
       </div>
+      {legend}
     </div>
   );
 };
