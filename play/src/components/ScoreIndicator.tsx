001483cd46705c6643b7656d3d3fae1bf983b59f
drewjhart <79417944+drewjhart@users.noreply.github.com>
2023-08-10 10:21:11 -0400

    updates animation per comments from Design team

--- play/src/components/ScoreIndicator.tsx
+++ play/src/components/ScoreIndicator.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import { Typography, Box } from '@mui/material';
 import { isNullOrUndefined } from '@righton/networking';
@@ -14,85 +14,88 @@ const ScorePill = styled('div')(({ theme }) => ({
   zIndex: 2,
 }));
 
-
-const ScoreAnimation = styled('div')({
+interface ScoreAnimationProps {
+  startAnimation: boolean;
+}
+const ScoreAnimation = styled('div', {
+  shouldForwardProp: (prop) => prop !== 'startAnimation',
+})<ScoreAnimationProps>(({ startAnimation }) => ({
   opacity: 1,
+  zIndex: 2,
+  animation: startAnimation ? `
+   scoreGrow 1000ms ease-in-out 0ms
+  `: ``,
+  '@keyframes scoreGrow': {
+      '0%, 100%': { 
+        opacity: 1,
+        transform: ' scale(1.0)' 
+      },
+      '50%': { 
+        opacity: 1,
+        transform: ' scale(1.2)' 
+      },
+    },
+}));
+
+const NewPointsPill = styled(ScorePill)(({ theme }) => ({
+  background: `${theme.palette.primary.altHighlightGradient}`,
+  zIndex: 2,
+}));
+
+const NewPointsAnimation = styled('div')({
+  opacity: 0,
   position: 'absolute',
   zIndex: 2,
   animation: `
-   scoreGrow 1000ms ease-in-out 0ms,
+   newScoreUpWiggle 1500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+   newScoreUpBounce 300ms ease-in-out 1500ms, 
+   newScoreUpFadeDown 1000ms ease-in-out 1800ms
   `,
-  '@keyframes scoreGrow': {
+  '@keyframes newScoreUpWiggle': {
       '0%': { 
-        opacity: 1,
-        transform: 'scale(1.0)' 
+        transform: 'translateY(-110%) rotate(0deg) scale(1.0)' 
+      },
+      '10%': { 
+        opacity: 1, 
+        transform: 'translateY(-110%) rotate(0deg) scale(1.2)' 
+      },
+      '15%, 45%, 75%': { 
+        opacity: 1, 
+        transform: 'translateY(-110%) rotate(-12deg) scale(1.2)' 
+      },
+      '30%, 60%': { 
+        opacity: 1, 
+        transform: 'translateY(-110%) rotate(12deg) scale(1.2)' 
+      },
+      '90%': { 
+        opacity: 1, 
+        transform: 'translateY(-110%) rotate(0deg) scale(1.2)' 
       },
       '100%': { 
-        opacity: 1,
-        transform: 'scale(1.2)' 
+        opacity: 1, 
+        transform: 'translateY(-110%) rotate(0deg) scale(1.0)' 
+      },
+    },
+    '@keyframes newScoreUpBounce': {
+      '0%, 100%': {
+        opacity: 1, 
+        transform: 'translateY(-110%)' 
+      },
+      '50%': {
+        opacity: 1, 
+        transform: 'translateY(-130%)' 
+      },
+    },
+    '@keyframes newScoreUpFadeDown': {
+      '0%': {
+        opacity: 1, 
+        transform: 'translateY(-110%)' 
+      },
+      '100%': { 
+        transform: 'translateY(0%)' 
       },
     },
 });
-
-// const NewPointsPill = styled(ScorePill)(({ theme }) => ({
-//   background: `${theme.palette.primary.altHighlightGradient}`,
-//   zIndex: 2,
-// }));
-
-// const NewPointsAnimation = styled('div')({
-//   opacity: 0,
-//   position: 'absolute',
-//   zIndex: 2,
-//   animation: `
-//    newScoreUpWiggle 1500ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-//    newScoreUpBounce 300ms ease-in-out 1500ms, 
-//    newScoreUpFadeDown 1000ms ease-in-out 1800ms
-//   `,
-//   '@keyframes newScoreUpWiggle': {
-//       '0%': { 
-//         transform: 'translateY(-110%) rotate(0deg) scale(1.0)' 
-//       },
-//       '10%': { 
-//         opacity: 1, 
-//         transform: 'translateY(-110%) rotate(0deg) scale(1.2)' 
-//       },
-//       '15%, 45%, 75%': { 
-//         opacity: 1, 
-//         transform: 'translateY(-110%) rotate(-12deg) scale(1.2)' 
-//       },
-//       '30%, 60%': { 
-//         opacity: 1, 
-//         transform: 'translateY(-110%) rotate(12deg) scale(1.2)' 
-//       },
-//       '90%': { 
-//         opacity: 1, 
-//         transform: 'translateY(-110%) rotate(0deg) scale(1.2)' 
-//       },
-//       '100%': { 
-//         opacity: 1, 
-//         transform: 'translateY(-110%) rotate(0deg) scale(1.0)' 
-//       },
-//     },
-//     '@keyframes newScoreUpBounce': {
-//       '0%, 100%': {
-//         opacity: 1, 
-//         transform: 'translateY(-110%)' 
-//       },
-//       '50%': {
-//         opacity: 1, 
-//         transform: 'translateY(-130%)' 
-//       },
-//     },
-//     '@keyframes newScoreUpFadeDown': {
-//       '0%': {
-//         opacity: 1, 
-//         transform: 'translateY(-110%)' 
-//       },
-//       '100%': { 
-//         transform: 'translateY(0%)' 
-//       },
-//     },
-// });
 
 interface ScoreIndicatorProps {
   newPoints?: number;
@@ -104,30 +107,25 @@ export default function ScoreIndicator({
   score,
 }: ScoreIndicatorProps) {
   const [newScore, setNewScore] = useState(score);
-  // adds an eventLister to add the new points to the existing score when the animation completes
-  useEffect(() => {
-    const element = document.getElementById('scoreAnimation');
-    const handleAnimationEnd = () => {
-      if (newPoints && newPoints > 0) {
-        setNewScore(score + newPoints);
-      }
-    };
-    element?.addEventListener('animationend', handleAnimationEnd);
-    return () => {
-      element?.removeEventListener('animationend', handleAnimationEnd);
-    };
-  }, [newPoints]); // eslint-disable-line react-hooks/exhaustive-deps
+  const [startScoreAnimation, setStartScoreAnimation] = useState(false);
+  
+  const handlePointsAnimationEnd = (event: React.AnimationEvent) => {
+    if (event.animationName === 'newScoreUpFadeDown' && newPoints && newPoints > 0) {
+      setNewScore(score + newPoints);
+      setStartScoreAnimation(true);
+    }
+  };
 
   return (
     <Box>
-      {/* <NewPointsAnimation id="newPointsAnimation">
-        {newPoints && newPoints > 0 ? (
-          <NewPointsPill>
+      { newPoints && newPoints > 0 ? (
+      <NewPointsAnimation onAnimationEnd={handlePointsAnimationEnd}>
+        <NewPointsPill>
             <Typography variant="overline">{`+${newPoints}`}</Typography>
-          </NewPointsPill>
-        ) : null}
-      </NewPointsAnimation>  */}
-      <ScoreAnimation id="scoreAnimation">
+        </NewPointsPill>
+      </NewPointsAnimation> 
+       ) : null}
+      <ScoreAnimation startAnimation={startScoreAnimation}>
         <ScorePill>
           <Typography data-testid="scoreindicator-newpoints" variant="overline">
             {isNullOrUndefined(newScore) ? 0 : newScore}
