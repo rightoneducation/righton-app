6d939deb90b4e4eba44730ecfaf94d085134860d
Alexandra-Kushnirsky <kushnirsky.alexandra@gmail.com>
2023-12-14 16:26:36 -0500

    Removed comment

--- play/src/components/ConfidenceMeterCard.tsx
+++ play/src/components/ConfidenceMeterCard.tsx
@@ -87,7 +87,7 @@ export default function ConfidenceMeterCard({
       </Typography>
     </Box>
   );
-  console.log(isSmallDevice)
+
   const responseOption = (option: IConfidenceOption) => {
     return (
       <Box
