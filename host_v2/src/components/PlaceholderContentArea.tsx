ab895011d714bad2f4815a44f1f9b6c0529c084a
Alexandra-Kushnirsky <kushnirsky.alexandra@gmail.com>
2023-12-21 13:11:56 -0500

    Added QuestionCard with styling

--- host_v2/src/components/PlaceholderContentArea.tsx
+++ host_v2/src/components/PlaceholderContentArea.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import { BodyContentAreaDoubleColumnStyled } from '../lib/styledcomponents/layout/BodyContentAreasStyled';
 import Card from './Card';
+import QuestionCard from './QuestionCard';
 
-interface PlaceholderContentAreaProps {} // eslint-disable-line
+interface PlaceholderContentAreaProps { } // eslint-disable-line
+// TODO: delete later <--------
+const sampleUrl = "https://images.unsplash.com/photo-1609188944094-394637c26769?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
+const sampleText = "A pair of shoes were 10% off last week. This week, theres an additional sale, and you can get an extra 40% off the already discounted price from last week. What is the total percentage discount that youd get if you buy the shoes this week?";
+// TODO: delete later <--------
 
-export default function PlaceholderContentArea({}: PlaceholderContentAreaProps) {
+export default function PlaceholderContentArea({ }: PlaceholderContentAreaProps) {
   // eslint-disable-line
   return (
     <BodyContentAreaDoubleColumnStyled container style={{ paddingTop: '16px' }}>
       <Grid item xs={12} sm={6} sx={{ width: '100%', height: '100%' }}>
-        <Card />
+        <QuestionCard questionText={sampleText} imageUrl={sampleUrl} />
         <Card />
       </Grid>
       <Grid item xs={12} sm={6} sx={{ width: '100%', height: '100%' }}>
