b3600c4f85b0543038c84f1b741a7065b5b87521
Alexandra-Kushnirsky <kushnirsky.alexandra@gmail.com>
2024-01-11 19:24:27 -0500

    Added a styled bar component to attempt to resolve default blue highlight

--- host_v2/src/components/ConfidenceComponents/CustomBar.tsx
+++ host_v2/src/components/ConfidenceComponents/CustomBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Bar } from 'victory';
-import { useTheme } from '@mui/material/styles';
+import { useTheme, styled } from '@mui/material/styles';
 
 // TODO: figure out what to do about keeping graph as 'confidence' instead of 
 // current toggle functionality based on click behavior
@@ -17,6 +17,22 @@ interface BarProps {
   graphClickInfo: GraphClickInfo;
   handleGraphClick: ({ graph, selectedIndex }: { graph: string | null; selectedIndex: number | null; }) => void;
 }
+
+const StyledBar = styled(Bar)(({ theme }) => ({
+  '-webkit-tap-highlight-color': `${theme.palette.primary.graphAccentColor} !important `,
+  '-webkit-touch-callout': 'none',
+  '-webkit-user-select': 'none',
+  '-khtml-user-select': 'none',
+  '-moz-user-select': 'none',
+  '-ms-user-select': 'none',
+  'user-select': 'none',
+  highlight: {
+    '&:hover': {
+      fill: 'rgba(255, 255, 255, 0.2)',
+    },
+  },
+}));
+
 
 export default function CustomBar(props: BarProps) {
   const {
@@ -35,7 +51,7 @@ export default function CustomBar(props: BarProps) {
 
   return (
     <g style={{ pointerEvents: 'visible' }}>
-      <Bar {...props} />
+      <StyledBar {...props} />
       <rect
         x={x !== undefined ? x - offset : - offset}
         y={graphTitleOffset}
