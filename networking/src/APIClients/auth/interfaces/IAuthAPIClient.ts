5be5abbd513ff0440b1ab0416cf80fc82578a5e6
drewjhart <79417944+drewjhart@users.noreply.github.com>
2024-05-30 09:28:52 -0400

    changes username set up so that username is visible for games

--- networking/src/APIClients/auth/interfaces/IAuthAPIClient.ts
+++ networking/src/APIClients/auth/interfaces/IAuthAPIClient.ts
@@ -5,7 +5,7 @@ export interface IAuthAPIClient {
   configAmplify(awsconfig: any): void;
   authEvents(payload: any): void;
   authListener(): void;
-  awsSignUp(email: string, password: string): void;
+  awsSignUp(username: string, email: string, password: string): void;
   awsConfirmSignUp(email: string, code: string): void;
   awsSignIn(email: string, password: string): void;
   awsSignInFederated(): void;
