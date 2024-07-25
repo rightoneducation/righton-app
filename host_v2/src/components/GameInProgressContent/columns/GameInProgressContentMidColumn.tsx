b24517842761773953d3a5ba39fc1f1be0a4a27e
drewjhart <79417944+drewjhart@users.noreply.github.com>
2024-07-25 18:42:06 -0300

    progress

--- host_v2/src/components/GameInProgressContent/columns/GameInProgressContentMidColumn.tsx
+++ host_v2/src/components/GameInProgressContent/columns/GameInProgressContentMidColumn.tsx
@@ -54,7 +54,7 @@ export default function GameInProgressContentMidColumn ({
           handleGraphClick={handleGraphClick}
         />
       }
-      {isHintEnabled && 
+      {isHintEnabled && currentPhase === IPhase.TWO &&
         <HintsCard 
           hints={currentHints}
           numPlayers={numPlayers}
