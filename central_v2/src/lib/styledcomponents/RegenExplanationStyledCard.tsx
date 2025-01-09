e5b1a627773dedea0ecaf8b5908963589118a2d4
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-01-09 15:53:33 -0500

    formatting

--- central_v2/src/lib/styledcomponents/RegenExplanationStyledCard.tsx
+++ central_v2/src/lib/styledcomponents/RegenExplanationStyledCard.tsx
@@ -1,6 +1,6 @@
 import { Box, styled } from '@mui/material';
 
-export const RegenExplanationStyledCard = styled(Box)(({ theme }) => ({
+export const RegenExplanationStyledCard = styled(Box)(({ theme }) => ({ // eslint-disable-line
   background: `${theme.palette.primary.aiGradient}`,
   width: '100%',
   paddingTop: '10px',
