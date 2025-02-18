123c6e3af2cb7d164555726acada1305c23c9815
Muhammad Qureshi <73972472+CCosmoe@users.noreply.github.com>
2025-02-18 15:11:55 -0600

    networkingneedswork

---
+++ networking/amplify/backend/function/UserCleaner/src/index.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+import AWS from 'aws-sdk'; // Use ES Module syntax
+
+// Initialize the S3 client
+
+export const handler = async (event) => {
+
+};
