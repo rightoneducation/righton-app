08ff08df29f066cc12496a7db4fc2c0e06211621
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-04-29 11:18:33 -0400

    amplify auth changes to add first_name and last_name

--- networking/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ networking/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,10 +10,15 @@ export type AmplifyDependentResourcesAttributes = {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
       "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "authusersGroupRole": "string"
     }
   },
   "function": {
