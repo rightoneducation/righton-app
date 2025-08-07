320e9630b36e64f68e4e82b268523f0a681c4f22
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-08-07 17:10:44 +0700

    lazy load for articles

--- networking/src/APIClients/cms/CMSQueries.ts
+++ networking/src/APIClients/cms/CMSQueries.ts
@@ -18,6 +18,28 @@ export const FETCH_ALL_ARTICLES = `*[_type == "article" || _type == "videoArticl
   readingTimeMinutes
   }`;
 
+export const FETCH_ARTICLES_PAGINATED = `*[_type == "article" || _type == "videoArticle"] | order(date desc) [$start...$end] {
+  _id,
+  image{
+    asset{
+      _ref
+    }
+  },
+  title,
+  youtubeLink,
+  tags,
+  date,
+  caption,
+  author,
+  affiliation,
+  contact,
+  details,
+  monsterSelect,
+  readingTimeMinutes
+}`;
+
+export const FETCH_ARTICLES_COUNT = `count(*[_type == "article" || _type == "videoArticle"])`;
+
   export const FETCH_ALL_CORNERSTONES = `*[(_type == "article" || _type == "videoArticle") && isCornerstone == true]{
     _id,
     image{
