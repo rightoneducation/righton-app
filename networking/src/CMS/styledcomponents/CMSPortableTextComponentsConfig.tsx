4cca6e1edb444da6aeff2f2484f4f4e5786d9c92
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-09-03 14:29:48 -0700

    fixes font color on articles

--- networking/src/CMS/styledcomponents/CMSPortableTextComponentsConfig.tsx
+++ networking/src/CMS/styledcomponents/CMSPortableTextComponentsConfig.tsx
@@ -3,11 +3,41 @@
 
   export const PortableTextComponentsConfig: PortableTextComponents = {
     block: {
-      h3: ({children}) => (
+      h1: ({ children }) => (
         <CMSHeaderText>{children}</CMSHeaderText>
       ),
-      normal: ({children}) => (
+      h2: ({ children }) => (
+        <CMSHeaderText>{children}</CMSHeaderText>
+      ),
+      h3: ({ children }) => (
+        <CMSHeaderText>{children}</CMSHeaderText>
+      ),
+      normal: ({ children }) => (
         <CMSBodyText>{children}</CMSBodyText>
+      ),
+    },
+    list: {
+      bullet: ({ children }) => (
+        <ul style={{ color: '#FFFFFF' }}>
+          {children}
+        </ul>
+      ),
+      number: ({ children }) => (
+        <ol style={{ color: '#FFFFFF' }}>
+          {children}
+        </ol>
+      ),
+    },
+    listItem: {
+      bullet: ({ children }) => (
+        <li>
+          <CMSBodyText>{children}</CMSBodyText>
+        </li>
+      ),
+      number: ({ children }) => (
+        <li>
+          <CMSBodyText>{children}</CMSBodyText>
+        </li>
       ),
     },
     hardBreak: () => <br />,
