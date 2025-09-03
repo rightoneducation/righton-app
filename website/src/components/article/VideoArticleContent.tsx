5bd54479892256de9383c0c31669306a290f38a0
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-09-03 13:53:56 -0700

    fixes video regex

--- website/src/components/article/VideoArticleContent.tsx
+++ website/src/components/article/VideoArticleContent.tsx
@@ -13,7 +13,7 @@ interface VideoArticleContainerInterface {
 
 const getYouTubeId = (url: string): string | null => {
   const regex =
-    /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\\s]{11})/;
+    /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/\s]{11})/;
   const match = url.match(regex);
   return match ? match[1] : null;
 };
