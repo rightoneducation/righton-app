6f2cfe22a7b062437d70225d022377639c528fea
drewjhart <79417944+drewjhart@users.noreply.github.com>
2025-09-24 18:16:20 -0300

    updates sanity templates

--- networking/sanity/schemaTypes/index.ts
+++ networking/sanity/schemaTypes/index.ts
@@ -1,4 +1,6 @@
 import { articleType } from "./articleType"
 import { videoArticleType } from "./videoArticleType"
+import { outsideResource } from "./outsideResource"
+import { rightOnResource } from "./rightOnResource"
 
-export const schemaTypes = [articleType, videoArticleType];
+export const schemaTypes = [articleType, videoArticleType, outsideResource, rightOnResource];
