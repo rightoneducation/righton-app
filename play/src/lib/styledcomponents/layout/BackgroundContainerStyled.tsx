1ed7b83ec170c1da55563f3b333a34559494f2dc
drewjhart <79417944+drewjhart@users.noreply.github.com>
2026-04-30 15:03:55 +0800

    implements mathsymbols on background

--- play/src/lib/styledcomponents/layout/BackgroundContainerStyled.tsx
+++ play/src/lib/styledcomponents/layout/BackgroundContainerStyled.tsx
@@ -1,14 +1,20 @@
 import { styled } from '@mui/material/styles';
 import { Stack } from '@mui/material';
-import NewPlayBackground from '../../../img/NewPlayBackground.svg';
+import mathSymbolsBackground from '../../../img/mathSymbolsBackground.svg';
+
 
 export default styled(Stack)(({ theme }) => ({
   height: '100%',
   position: 'fixed', // 100%, fixed to prevent sizing changes on mobile based on url bar etc
   width: '100%',
-  background: `${theme.palette.primary.splashScreenBackgroundGradient} no-repeat`,
-  backgroundSize: `100%`,
-  backgroundPosition: 'center top',
+  // background: `${theme.palette.primary.splashScreenBackgroundGradient} no-repeat`,
+  backgroundImage: `
+    linear-gradient(180deg, rgba(42, 18, 79, 0.95), rgba(96, 41, 181, 0.95)),
+    url(${mathSymbolsBackground})
+  `,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'bottom', // Adjust as needed
+  backgroundSize: `100% 100%, auto 100%`,
   backgroundAttachment: 'fixed',
   display: 'flex',
   alignItems: 'center',
