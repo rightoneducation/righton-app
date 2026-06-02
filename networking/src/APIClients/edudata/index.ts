4b1c58e4de0a82ab94635445bb721ddc2d017db5
drewjhart <79417944+drewjhart@users.noreply.github.com>
2026-06-02 09:25:35 +0800

    updates

--- networking/src/APIClients/edudata/index.ts
+++ networking/src/APIClients/edudata/index.ts
@@ -1,6 +1,6 @@
 // created from 'create-ts-index'
 
 export * from './EduDataAPIClient';
+export * from './config';
 export * from './interfaces';
 export * from './util';
-export * from './config';
